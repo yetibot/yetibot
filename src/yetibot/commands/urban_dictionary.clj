49fb431b57d454fc00bbd071a5ea532bea3f3641
Trevor Hartman <trevorhartman@gmail.com>
2012-07-18 14:25:19 -0700

    Add "urban random" support

--- src/yetibot/commands/urban_dictionary.clj
+++ src/yetibot/commands/urban_dictionary.clj
@@ -4,16 +4,29 @@
             [clojure.data.json :as json])
   (:use [yetibot.util]))
 
-(def endpoint "http://api.urbandictionary.com/v0/define?term=")
+(def endpoint "http://api.urbandictionary.com/v0/")
 
 (defn search-urban-dictionary
   [q]
-  (let [uri (str endpoint q)]
-    (println (str "hitting" uri))
+  (let [uri (str endpoint "define?term=" q)]
     (with-open [client (client/create-client)]
       (let [response (client/GET client uri)]
         (client/await response)
         (json/read-json (client/string response))))))
+
+(defn fetch-random []
+  (let [uri (str endpoint "random")]
+    (with-open [client (client/create-client)]
+      (let [response (client/GET client uri)]
+        (client/await response)
+        (json/read-json (client/string response))))))
+
+
+(defn random-cmd
+  "urban random # fetch a random definition from Urban Dictionary"
+  []
+  (first
+    (for [i (:list (fetch-random))] [(:word i) (:definition i)])))
 
 (defn search-cmd
   "urban <query> # search for <query> on Urban Dictionary"
@@ -22,4 +35,5 @@
     (for [i (:list (search-urban-dictionary q))] (:definition i))))
 
 (cmd-hook #"urban"
+          #"^random" (random-cmd)
           #".*" (search-cmd p))
