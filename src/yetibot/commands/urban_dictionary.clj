0f4d7153faa8fba5cb320644d545c463cdd622cd
Trevor Hartman <trevorhartman@gmail.com>
2012-07-18 14:05:17 -0700

    Add urban dictionary support

---
+++ src/yetibot/commands/urban_dictionary.clj
@@ -0,0 +1,25 @@
+(ns yetibot.commands.urban-dictionary
+  (:require [clojure.string :as s]
+            [http.async.client :as client]
+            [clojure.data.json :as json])
+  (:use [clj-ssh.ssh]
+        [yetibot.util]))
+
+(def endpoint "http://api.urbandictionary.com/v0/define?term=")
+
+(defn search-urban-dictionary
+  [q]
+  (let [uri (str endpoint q)]
+    (println (str "hitting" uri))
+    (with-open [client (client/create-client)]
+      (let [response (client/GET client uri)]
+        (client/await response)
+        (json/read-json (client/string response))))))
+
+(defn search-cmd
+  "urban <query> # search for <query> on Urban Dictionary"
+  [q]
+  (for [i (:list (search-urban-dictionary q))] (:definition i)))
+
+(cmd-hook #"urban"
+          #".*" (search-cmd p))
