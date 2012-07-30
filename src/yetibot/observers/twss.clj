06fed563bea92ef9a074833d5bf076404dc09ab2
Trevor Hartman <trevorhartman@gmail.com>
2012-07-30 16:36:06 -0700

    Limit twss to once-per-hour

--- src/yetibot/observers/twss.clj
+++ src/yetibot/observers/twss.clj
@@ -2,7 +2,7 @@
   (:require [robert.hooke :as rh]
             [yetibot.campfire :as cf]
             [clojure.contrib.string :as s]
-            [useful.fn :as useful]
+            [useful.fn :as useful :only rate-limited]
             [yetibot.core :as core]
             [clojure.xml :as xml])
   (:use [yetibot.util]))
@@ -12,9 +12,14 @@
 ; observers hook handle-text-message which is called before
 ; the more specific handle-command
 
+(def one-hour 3600000)
 (def endpoint "http://twss-classifier.heroku.com/?sentence=")
 (def false-phrase "Not TWSS")
 (def reply "That's what she said")
+
+(def reply-once-per-hour
+  (useful/rate-limited
+    #((cf/send-message (str reply))) one-hour))
 
 (defn load-twss [phrase]
   (xml/parse (str endpoint (encode phrase))))
@@ -24,7 +29,7 @@
         result (first (for [el xs :when (= (:id (:attrs el)) "result")] el))]
     (if (s/substring? false-phrase (str result))
       false
-      (cf/send-message (str reply)))))
+      (reply-once-per-hour))))
 
 (obs-hook
   ["TextMessage" "PasteMessage"]
