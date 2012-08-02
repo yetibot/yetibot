beea74cd6d79c382220c684ad55ec8f7db7adc88
Ryan Eiger <eiger.ryan@gmail.com>
2012-08-02 14:44:19 -0700

    Added uptime command

---
+++ src/yetibot/commands/uptime.clj
@@ -0,0 +1,16 @@
+(ns yetibot.commands.uptime
+  (:import org.apache.commons.lang3.time.DurationFormatUtils)
+  (:use [yetibot.util :only(cmd-hook)]))
+
+(def start-time (System/currentTimeMillis))
+
+(defn now [] (System/currentTimeMillis))
+
+(defn uptime-millis [] (- (now) start-time))
+
+(defn uptime-cmd
+  "uptime # list uptime in milliseconds"
+  [] (DurationFormatUtils/formatDurationWords (uptime-millis) true true))
+
+(cmd-hook #"uptime"
+          #"" (uptime-cmd))
