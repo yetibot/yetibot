7f1c64802650e258ffdd6b367e189279c4ec0941
Trevor Hartman <trevorhartman@gmail.com>
2012-08-09 13:14:43 -0700

    Limit chatoms observer to one per 5 minutes

--- src/yetibot/observers/chatoms.clj
+++ src/yetibot/observers/chatoms.clj
@@ -1,13 +1,16 @@
 (ns yetibot.observers.chatoms
   (:use [yetibot.util :only (chat-result obs-hook)]
-         [yetibot.util.http :only (get-json)]))
+        [useful.fn :only (rate-limited)]
+        [yetibot.util.http :only (get-json)]))
 
 (def uri "http://chatoms.com/chatom.json?Normal=1&Fun=20&Philosophy=3&Out+There=4&Love=5&Personal=10")
 
-(defn report-chatom
-  []
-  (chat-result
-    (:text (get-json uri))))
+(def five-minutes 300000)
+
+(def report-chatom
+  (rate-limited
+    #((chat-result (:text (get-json uri))))
+    five-minutes))
 
 (obs-hook
   ["KickMessage" "IdleMessage" "LeaveMessage" "TimestampMessage"
