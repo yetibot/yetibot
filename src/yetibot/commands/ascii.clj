1fb9717786c4f4467a3cfd6fb2d812f55db4f2cc
Trevor Hartman <trevorhartman@gmail.com>
2012-08-09 13:06:51 -0700

    Encode param for \!ascii

--- src/yetibot/commands/ascii.clj
+++ src/yetibot/commands/ascii.clj
@@ -1,13 +1,13 @@
 (ns yetibot.commands.ascii
   (:use [yetibot.util :only [cmd-hook]]
-        [yetibot.util.http :only [fetch]]))
+        [yetibot.util.http :only [fetch encode]]))
 
 (def endpoint "http://asciime.heroku.com/generate_ascii?s=")
 
 (defn ascii
   "ascii <text> # generates ascii art representation of <text>"
   [text]
-  (fetch (str endpoint text)))
+  (fetch (str endpoint (encode text))))
 
 
 (cmd-hook #"ascii"
