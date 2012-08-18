c563c2a7294164ad3c003465b24c5e88d69a7385
Trevor Hartman <trevorhartman@gmail.com>
2012-08-17 17:21:25 -0700

    Add !info to query DuckDuckGo; fix #44

---
+++ src/yetibot/commands/info.clj
@@ -0,0 +1,16 @@
+(ns yetibot.commands.info
+  (:use [yetibot.util :only (cmd-hook)]
+        [yetibot.util.http :only (get-json encode)]))
+
+(def endpoint "http://api.duckduckgo.com/?format=json&q=")
+
+(defn info
+  "info <topic> # retrieve info about <topic> from DuckDuckGo"
+  [topic]
+  (let [json (get-json (str endpoint topic))]
+    (if-let [resp (-> json :RelatedTopics first :Text)]
+      resp
+      (str "No info available for " topic))))
+
+(cmd-hook #"info"
+          #".+" info)
