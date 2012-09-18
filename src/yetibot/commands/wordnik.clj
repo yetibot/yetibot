6fb98f2db12763f3176b76331a0174bf90e9e5fd
Trevor Hartman <trevorhartman@gmail.com>
2012-09-18 11:46:27 -0700

    Add random to WordNik

--- src/yetibot/commands/wordnik.clj
+++ src/yetibot/commands/wordnik.clj
@@ -7,6 +7,9 @@
 
 (def ^:private api-key (System/getenv "WORDNIK_API_KEY"))
 
+(defmacro ^:private with-auth [& body]
+  `(with-api-key api-key ~@body))
+
 (defn- extract-definitions-text [ds]
   (map-indexed (fn [idx d] (str (inc idx) ". " (:text d)))
                ds))
@@ -18,16 +21,27 @@
 (defn define
   "wordnik define <word> # look up the definition for <word> on Wordnik"
   [w]
-  (with-api-key
-    api-key
-    (extract-definitions-text (word/definitions (s/trim w)))))
+  (with-auth
+    (let [ds (word/definitions (s/trim w))
+          word (-> ds first :word)]
+      (if word
+        (conj (extract-definitions-text ds)
+              word)
+        (str "No defitions found for " )))))
+
+(defn random
+  "wordnik random # look up a random word on Wordnik"
+  []
+  (with-auth
+    (define (:word (words/random-word)))))
 
 (defn wotd
   "wordnik wotd # look up the Word of the Day on Wordnik"
   []
-  (with-api-key api-key
-                (format-defs (words/wotd))))
+  (with-auth
+    (format-defs (words/wotd))))
 
 (cmd-hook #"wordnik"
           #"define\s(\w+)" (define (second p))
+          #"random" (random)
           #"wotd" (wotd))
