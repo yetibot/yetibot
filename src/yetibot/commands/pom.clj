21e5c90f8fb4152e911516db40ced7c535b7aca2
Trevor Hartman <trevorhartman@gmail.com>
2012-10-03 14:16:07 -0700

    Add poms command for parsing poms in configured GitHub organization

---
+++ src/yetibot/commands/pom.clj
@@ -0,0 +1,118 @@
+(ns yetibot.commands.pom
+  (:require [clojure.string :as s]
+            [yetibot.api.github :as gh]
+            [clojure.data.xml :as xml]
+            [clojure.zip :as zip])
+  (:use [yetibot.util :only (cmd-hook)]
+        [clojure.data.zip.xml :only (attr text xml->)]))
+
+(def tree-art "└──") ;;; "├── "
+
+(defn parse-pom
+  "Returns xml-zip'd pom"
+  [pom]
+  (zip/xml-zip (xml/parse (java.io.StringReader. pom))))
+
+(defn poms-by-path
+  "Return a map of path keys and xml-zip'd content values for all found poms in a given repo"
+  [repo]
+  (let [tree-paths (gh/find-paths (gh/tree repo) #"pom.xml")]
+    (prn "looking up poms for " (map :path tree-paths))
+    (zipmap (map :path tree-paths)
+            (->> tree-paths
+              (map #(gh/raw repo (:path %)))
+              (map :body)
+              (map parse-pom)))))
+
+(defn sort-pom-list
+  "Sort a map of poms by how many slashes appear in its key"
+  [poms]
+  (sort-by (fn [[k, v]] (count (filter #(= \/ %) k))) poms))
+
+(defn extract-from-pom
+  "Looks up poms for a given repo, and passes it to an extractor fn"
+  [repo extractor]
+  (let [poms (->> repo
+               poms-by-path
+               sort-pom-list)]
+    (extractor poms)))
+
+(defn indent-n
+  [n] (s/join "" (repeat (dec n) "    ")))
+
+; expects a tree with first item as key, 2nd and value, and third as children
+; ["pom.xml" "2.2.2" [["somedep" "2092"] ["dep2" "12312"]]]
+(defn format-tree-data
+  ([tree] (format-tree-data tree 1))
+  ([tree lvl]
+   (prn tree lvl)
+   (let [[k v children] tree]
+     (prn k v children)
+     (str
+       (indent-n lvl) tree-art " " k ": " v
+       (when (seq children)
+         (str \newline
+              (s/join \newline
+                      (map #(format-tree-data % (inc lvl)) children))))))))
+
+(defn extract-version
+  "A pom can either have its own version or it can specify a parent pom. Look for the
+  version first, and if it's blank look for the parent"
+  [poms]
+  (for [[path, pom] poms]
+    (let [version (xml-> pom :version text)
+          version-str (if (seq version)
+                        (first version)
+                        (format "parent/%s"
+                                (first (xml-> pom :parent :version text))))]
+      [path version-str])))
+
+(defn pom-dep-node
+  [pom node]
+  (xml-> pom :dependencies :dependency node text))
+
+(defn deps-as-tree-nodes
+  [pom]
+  (let [dep-fn (partial pom-dep-node pom)
+        [group-ids artifact-ids versions] (map dep-fn [:groupId :artifactId :version])
+        deps (map vector group-ids artifact-ids versions)]
+    (map (fn [[gid aid v]] [(format "%s/%s" gid aid) v]) deps)))
+
+(defn extract-version-and-deps
+  [poms]
+  (let [with-versions (extract-version poms)
+        version-and-pom (map conj with-versions (vals poms))]
+    (for [[path version-str pom] version-and-pom]
+      (let [deps (deps-as-tree-nodes pom)]
+        [path version-str deps]))))
+
+(defn formatted-tree
+  [repo fn]
+  (str repo \newline
+       (s/join
+         \newline
+         (map format-tree-data
+              (extract-from-pom repo fn)))))
+
+;;; example:
+;;; com.foo.api
+;;; └── pom.xml: 7.1.7-SNAPSHOT
+;;; └── api-client/pom.xml: parent/7.1.7-SNAPSHOT
+;;; └── api-ws/pom.xml: parent/7.1.7-SNAPSHOT
+(defn poms-for-repo
+  "poms <repo> # extract versions from poms in <repo>"
+  [repo]
+  (formatted-tree repo extract-version))
+
+;;; example:
+;;; com.foo.api
+;;; └── pom.xml: 7.1.7-SNAPSHOT
+;;;     └── com.twitter/util-collection: 5.3.10
+(defn deps-for-repo
+  "poms deps <repo> # show pom versions along with its dependencies for <repo>"
+  [repo]
+  (formatted-tree repo extract-version-and-deps))
+
+(cmd-hook #"poms"
+          #"deps (\S+)" (deps-for-repo (second p))
+          #"(\S+)" (poms-for-repo (second p)))
