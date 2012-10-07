d37395df7963d5dd7716baa0b1f4ad2ef160b463
Trevor Hartman <trevorhartman@gmail.com>
2012-10-07 15:33:17 -0700

    Add  command to eval Clojure in YetiBot's context, locked down to privileged users

---
+++ src/yetibot/commands/eval.clj
@@ -0,0 +1,18 @@
+(ns yetibot.commands.eval
+  (:use [yetibot.util :only (cmd-hook env)]
+        [clojure.string :only (split)]))
+
+(def ^:private privs (set (map read-string (split (or (:YETIBOT_EVAL_PRIVS env) "") #","))))
+
+(defn- user-is-allowed? [user]
+  (boolean (some #{(:id user)} privs)))
+
+(defn eval-cmd
+  "eval <form> # evaluate the <form> data structure in YetiBot's context"
+  [form user]
+  (if (user-is-allowed? user)
+    (eval (read-string form))
+    (format "You are not allowed, %s." (:name user))))
+
+(cmd-hook #"eval"
+          _ (eval-cmd p user))
