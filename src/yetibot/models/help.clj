a9c752a973397e7b81fddb0bf9fc9b5efca70a7c
Trevor Hartman <trevorhartman@gmail.com>
2012-10-08 12:02:02 -0700

    Store help docs as a set to ensure there are no dupes

--- src/yetibot/models/help.clj
+++ src/yetibot/models/help.clj
@@ -9,9 +9,7 @@
 (defn add-docs [prefix cmds]
   (println (str "adding docs for " prefix \newline (apply str cmds)))
   ; add to the docs atom using prefix string as the key
-  (swap! docs conj {(str prefix) 
-                    (map s/trim
-                     (remove nil? cmds))}))
+  (swap! docs conj {(str prefix) (set (map s/trim (remove nil? cmds)))}))
 
 (defn get-docs []
   @docs)
