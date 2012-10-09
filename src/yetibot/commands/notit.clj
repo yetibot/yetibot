f703091f343156fb4bef53a0e1c66c994a3114e6
Trevor Hartman <trevorhartman@gmail.com>
2012-10-09 12:13:59 -0700

    Fix mixed space/tab on notit.clj

--- src/yetibot/commands/notit.clj
+++ src/yetibot/commands/notit.clj
@@ -1,6 +1,6 @@
 (ns yetibot.commands.notit
-	(:require [yetibot.models.users :as users]
-						[clojure.set])
+  (:require [yetibot.models.users :as users]
+            [clojure.set])
   (:use [yetibot.util]))
 
 (def empty-it-message "Nobody has registered as not-it yet.")
@@ -9,8 +9,8 @@
 (defn get-its [] @its)
 
 (defn reset-its 
-	"notit reset # resets the current not-it list"
-	[] (reset! its #{}))
+  "notit reset # resets the current not-it list"
+  [] (reset! its #{}))
 
 (defn show-its
   "notit show # show the current list of users registered as not-it"
@@ -23,18 +23,18 @@
 (defn get-real-users [] (remove #{"YetiBot"} (users/get-user-names)))
 
 (defn register-not-it
-	"notit # register a user as not-it"
-	([user]
-		(let [user-name (:name user)
-					new-not-its (swap! its conj user-name)
-					set-real-users (set (get-real-users))
-					potential-its (clojure.set/difference set-real-users new-not-its)]
-		(if (= (count potential-its) 1)
-			(do 
-				(reset-its)
-				(str (first potential-its) " is it! Resetting not-its."))
-			"Hurry up or you're going to be it!"
-			))))
+  "notit # register a user as not-it"
+  ([user]
+   (let [user-name (:name user)
+         new-not-its (swap! its conj user-name)
+         set-real-users (set (get-real-users))
+         potential-its (clojure.set/difference set-real-users new-not-its)]
+     (if (= (count potential-its) 1)
+       (do 
+         (reset-its)
+         (str (first potential-its) " is it! Resetting not-its."))
+       "Hurry up or you're going to be it!"
+       ))))
 
 (cmd-hook #"notit"
           #"reset" (reset-its)
