fa5dda5b2c759e3a567bf542761bfe3452417a96
Trevor Hartman <trevorhartman@gmail.com>
2012-10-10 13:32:44 -0700

    Fix indentation

--- src/yetibot/util.clj
+++ src/yetibot/util.clj
@@ -20,38 +20,38 @@
 ; command hook
 (defmacro cmd-hook [prefix & exprs]
   (let [[topic prefix] (if (vector? prefix) prefix [(str prefix) prefix])]
-  `(do
-     (rh/add-hook
-       #'core/handle-command
-       (fn [~'callback ~'cmd ~'args ~'user ~'opts]
-         ; only match against the
-         ; first word in args
-         (if (re-find ~prefix (s/lower-case ~'cmd))
-           (do
-             (println (str "found " ~prefix " on cmd " ~'cmd ". args are:" ~'args))
-             ; try matching the available sub-commands
-             (cond-let [~'p]
-                       ; rebuild the pairs in `exprs` as valid input for cond-let
-                       ~@(map (fn [i#]
-                                (cond
-                                  ; prefix to match
-                                  (instance? Pattern i#) `(re-find ~i# ~'args)
-                                  ; placeholder - set p equal to the args (which
-                                  ; might by nil, so or it with :empty)
-                                  (= i# '_) `(or ~'args :empty)
-                                  ; send result back to handle-command
-                                  :else `~i#))
-                              exprs)
-                       ; default to help
-                       true (core/handle-command "help" (str ~prefix) ~'user ~'opts)))
-           (~'callback ~'cmd ~'args ~'user ~'opts))))
-     ; extract the meta from the commands and use it to build docs
-     (help/add-docs ~topic
-                    (map
-                      (fn [i#]
-                        (if (list? i#)
-                          (:doc (meta (resolve (first i#))))))
-                      '~exprs)))))
+    `(do
+       (rh/add-hook
+         #'core/handle-command
+         (fn [~'callback ~'cmd ~'args ~'user ~'opts]
+           ; only match against the
+           ; first word in args
+           (if (re-find ~prefix (s/lower-case ~'cmd))
+             (do
+               (println (str "found " ~prefix " on cmd " ~'cmd ". args are:" ~'args))
+               ; try matching the available sub-commands
+               (cond-let [~'p]
+                         ; rebuild the pairs in `exprs` as valid input for cond-let
+                         ~@(map (fn [i#]
+                                  (cond
+                                    ; prefix to match
+                                    (instance? Pattern i#) `(re-find ~i# ~'args)
+                                    ; placeholder - set p equal to the args (which
+                                    ; might by nil, so or it with :empty)
+                                    (= i# '_) `(or ~'args :empty)
+                                    ; send result back to handle-command
+                                    :else `~i#))
+                                exprs)
+                         ; default to help
+                         true (core/handle-command "help" (str ~prefix) ~'user ~'opts)))
+             (~'callback ~'cmd ~'args ~'user ~'opts))))
+       ; extract the meta from the commands and use it to build docs
+       (help/add-docs ~topic
+                      (map
+                        (fn [i#]
+                          (if (list? i#)
+                            (:doc (meta (resolve (first i#))))))
+                        '~exprs)))))
 
 ; observer hook
 (defn obs-hook
