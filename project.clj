9d131b1e7aeedaf0e55d772b34e6686d3f927580
Trevor Hartman <trevorhartman@gmail.com>
2012-10-14 17:17:27 -0700

    Upgrade to lein 2 :fallen_leaf:

--- project.clj
+++ project.clj
@@ -24,6 +24,5 @@
                            [org.clojure/data.xml "0.0.6"]
                            [org.clojure/data.zip "0.1.1"]
                            [clj-aws-s3 "0.3.2"]
-                           ]
-            :dev-dependencies [[clj-stacktrace "0.2.3"]]
+                           [environ "0.3.0"]]
             :main yetibot.core)
