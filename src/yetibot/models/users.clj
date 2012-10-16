22d09069d65e9d75b0d86e5d08b1e1f03df70f05
Ryan Eiger <eiger.ryan@gmail.com>
2012-10-16 14:41:35 -0700

    Improved notit command by tracking users who are active, and only considering them for notit

--- src/yetibot/models/users.clj
+++ src/yetibot/models/users.clj
@@ -1,13 +1,28 @@
-(ns yetibot.models.users)
+(ns yetibot.models.users
+  (:import java.util.Date)
+  (:import java.sql.Timestamp)
+  (:import java.text.SimpleDateFormat))
+
+(def active-threshold-minutes 15)
+(def active-threshold-milliseconds (* active-threshold-minutes 60 1000))
+(def campfire-date-pattern "yyyy/MM/dd HH:mm:ss Z")
+(def date-formatter (doto (new SimpleDateFormat campfire-date-pattern) (.setTimeZone (java.util.TimeZone/getTimeZone "GreenwichEtc"))))
+(def yetibot-id 1008539)
 
 (defonce users (atom {}))
 
 ;;; (defn add-user [id user]
 ;;;   (swap! user conj {id user}))
 
+(defn get-refreshed-user
+  "Returns an already existing user from the atom if available, otherwise a new user with a last_active timestamp"
+  [user]
+  (let [id (:id user)]
+    (get @users id (assoc user :last_active (.format date-formatter (new Date))))))
+
 (defn reset-users-from-room [room]
   (let [us (-> room :room :users)
-        us-by-id (into {} (for [u us] [(:id u) u]))]
+        us-by-id (into {} (for [u us] [(:id u) (get-refreshed-user u)]))]
     (reset! users us-by-id)))
 
 (defn get-user [id]
@@ -22,3 +37,27 @@
 
 (defn get-rand-user []
   (rand-nth (vals @users)))
+
+(defn get-user-ms [user] (.getTime (.parse date-formatter (:last_active user))))
+
+(defn is-active?
+  [user]
+  (if (contains? user :last_active)
+    (let [current-ms (.getTime (new Date))
+          ms-since-active (- current-ms (get-user-ms user))]
+      (< ms-since-active active-threshold-milliseconds))
+    false))
+
+(defn is-yetibot? [user] (= (:user_id user) yetibot-id))
+
+(defn get-active-users [] (filter is-active? (vals @users)))
+
+(defn get-active-humans [] (remove is-yetibot? (get-active-users)))
+
+(defn get-updated-user [id last_active]
+  (assoc (get-user id) :last_active last_active))
+
+(defn update-active-timestamp [{id :user_id last_active :created_at}]
+  (do
+    (swap! users conj {id (get-updated-user id last_active)})
+    (prn @users)))
