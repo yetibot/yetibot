5d61539aa6d4af712f64e607a5e5e44e5d13dde0
Trevor Hartman <trevorhartman@gmail.com>
2012-10-23 20:51:53 -0700

    Extract data formatting from chat-data-structure and move into separate yetibot.util.format namespace

---
+++ src/yetibot/util/format.clj
@@ -0,0 +1,37 @@
+(ns yetibot.util.format
+  (:require [clojure.string :as s])
+  (:import [clojure.lang Associative Sequential]))
+
+(defmulti ^:private format-flattened type)
+
+; send map as key: value pairs
+(defmethod format-flattened Associative [d]
+  (format-flattened (map #(str (first %1) ": " (second %1)) d)))
+
+(defmethod format-flattened Sequential [d]
+  (s/join \newline d))
+
+(prefer-method format-flattened Sequential Associative)
+
+; default handling for strings and other non-collections
+(defmethod format-flattened :default [d]
+  d)
+
+(defn format-data-structure
+  "returns a string representation of `d` as well as the fully-flattened data representation"
+  [d]
+  (if (and (not (map? d))
+           (coll? d)
+           (coll? (first d)))
+    ; if it's a nested sequence, recursively flatten it
+    (if (map? (first d))
+      ; merge if the insides are maps
+      (format-data-structure (apply merge-with d))
+      ; otherwise flatten
+      (format-data-structure (apply concat d)))
+    ; otherwise send in the most appropriate manner
+    [(format-flattened d) d]))
+
+(defn format-data-as-string [d]
+  (let [[s _] (format-data-structure d)]
+    s))
