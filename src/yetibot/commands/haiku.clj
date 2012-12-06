39ee19c431f30df81346c4a2d6475340c4f49b37
Trevor Hartman <trevorhartman@gmail.com>
2012-12-05 20:41:26 -0800

    Add haiku command; fix #122

---
+++ src/yetibot/commands/haiku.clj
@@ -0,0 +1,16 @@
+(ns yetibot.commands.haiku
+  (:require [clojure.xml :as xml]
+            [yetibot.hooks :refer [cmd-hook]]
+            [yetibot.util.http :refer [get-json]]))
+
+(def endpoint "http://www.randomhaiku.com/haiku.xml")
+
+(defn haiku
+  "haiku # fetch a random haiku"
+  {:test #(assert (seq? #cmd haiku))}
+  [_] (let [res (xml-seq (xml/parse endpoint))]
+        (for [el res :when (= :line (:tag el))]
+          (first (:content el)))))
+
+(cmd-hook #"haiku"
+          _ haiku)
