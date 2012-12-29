f79dd0a2fb569bc5d596a98bda3dccd187628a12
Trevor Hartman <trevorhartman@gmail.com>
2012-12-29 10:19:49 -0800

    Let users set their status and show the status list; fix #132

---
+++ src/yetibot/commands/status.clj
@@ -0,0 +1,30 @@
+(ns yetibot.commands.status
+  (:require [clj-time
+              [format :refer [formatter]]
+              [core :refer [default-time-zone]]
+              [local :refer [local-now format-local-time *local-formatters*]]]
+            [yetibot.hooks :refer [cmd-hook]]))
+
+(defonce statuses (atom {}))
+(def empty-msg "No one has set their status")
+
+(defn- fmt-local-time []
+  (binding [*local-formatters*
+             {:short-time (formatter "hh:mm aa" (default-time-zone))}]
+    (format-local-time (local-now) :short-time)))
+
+(defn set-status
+  "status <message> # update your status"
+  [{:keys [match user]}]
+  (let [st (format "%s at %s: %s" (:name user) (fmt-local-time) match)]
+    (swap! statuses conj {(:name user) st})
+    "Status set"))
+
+(defn show-status
+  "status # show everyone's status"
+  [_] (let [ss @statuses]
+        (if (empty? ss) empty-msg (vals ss))))
+
+(cmd-hook #"status"
+          #".+" set-status
+          _ show-status)
