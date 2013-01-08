8c285523d26b4404142bc7cda858f57aaf7c7e81
Trevor Hartman <trevorhartman@gmail.com>
2013-01-08 10:20:49 -0800

    Add search to wiki and fix encoding

--- src/yetibot/commands/wikipedia.clj
+++ src/yetibot/commands/wikipedia.clj
@@ -1,14 +1,15 @@
 (ns yetibot.commands.wikipedia
-  (:require [yetibot.util.http :refer [get-json]]
+  (:require [yetibot.util.http :refer [get-json encode]]
             [yetibot.hooks :refer [cmd-hook]]))
 
 (defn endpoint-search [q]
-  (str "http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=" q))
+  (str "http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=" 
+       (encode q)))
 
 (defn endpoint-summary [title]
   (str
     "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=true&format=json&exintro=true&titles="
-    title))
+    (encode title)))
 
 (defn has-match? [res]
   (and (not (empty? res))
@@ -21,6 +22,14 @@
   (let [res (get-json (endpoint-summary title))]
     (first (keep :extract (tree-seq map? vals res)))))
 
+(defn search-cmd
+  "wiki search <term> # search Wikipedia for titles"
+  [{[_ term] :match}]
+  (let [res (get-json (endpoint-search term))]
+    (if (has-match? res)
+      (second res)
+      (no-match term))))
+
 (defn wiki-cmd
   "wiki <term> # look up Wikipedia summary for <term>"
   [{:keys [match]}]
@@ -30,4 +39,5 @@
       (no-match match))))
 
 (cmd-hook ["wiki" #"^wiki(pedia)*"]
+          #"search\s+(\w+)" search-cmd
           #"\w+" wiki-cmd)
