e3423d8ff5b22d244a7855f87a40a28495e147a4
Trevor Hartman <trevorhartman@gmail.com>
2013-02-05 10:19:43 -0800

    Add github status command

--- src/yetibot/commands/github.clj
+++ src/yetibot/commands/github.clj
@@ -1,5 +1,6 @@
 (ns yetibot.commands.github
   (:require [yetibot.api.github :as gh]
+            [yetibot.util.http :refer [get-json]]
             [clojure.string :as s])
   (:use [yetibot.hooks :only [cmd-hook]]
         [yetibot.util :only [env]]))
@@ -17,7 +18,13 @@
   [{[_ repo] :match}]
   (map :name (gh/branches repo)))
 
+(defn status
+  "gh status # show GitHub's current system status"
+  [_] ((juxt :status :body :created_on)
+       (get-json "https://status.github.com/api/last-message.json")))
+
 (cmd-hook ["gh" #"^gh$"]
           #"feed" feed
           #"repos" repos
+          #"status" status
           #"branches\s+(\S+)" branches)
