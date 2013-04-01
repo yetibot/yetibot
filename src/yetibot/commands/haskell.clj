2c8cfd79bc67d5b6e3357202439dce654df9f10c
Trevor Hartman <trevorhartman@gmail.com>
2013-04-01 16:46:15 -0700

    Fix haskell regex bug

--- src/yetibot/commands/haskell.clj
+++ src/yetibot/commands/haskell.clj
@@ -5,10 +5,10 @@
 (def endpoint "http://tryhaskell.org/haskell.json?method=eval&random=0.6923271066043526&expr=")
 
 (defn haskell-cmd
-  "haskell <expression> # eval haskell expression"
+  "hs <expression> # evaluate haskell expression"
   [{:keys [args]}]
   (let [json (get-json (str endpoint (encode args)))]
     ((juxt :type :result) json)))
 
-(cmd-hook ["haskell" #"^haskell|hs$"]
+(cmd-hook #"hs"
           _ haskell-cmd)
