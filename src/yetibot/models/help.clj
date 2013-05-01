4f8feee1f76a506c1e675ef01b0d6cd5f249d7b6
Trevor Hartman <trevorhartman@gmail.com>
2013-04-30 20:58:07 -0700

    Remove unnecessary yetibot.core require in help

--- src/yetibot/models/help.clj
+++ src/yetibot/models/help.clj
@@ -1,12 +1,10 @@
 (ns yetibot.models.help
   (:require [clojure.string :as s]
-            [yetibot.core :as core]
             [clojure.data.json :as json]))
 
 (defonce docs (atom {}))
 
 (defn add-docs [prefix cmds]
-  (println (str "adding docs for " prefix \newline (apply str cmds)))
   ; add to the docs atom using prefix string as the key
   (swap! docs conj {(str prefix) (set (map s/trim (remove nil? cmds)))}))
 
