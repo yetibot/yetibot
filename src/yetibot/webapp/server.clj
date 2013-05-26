75dee352fb148c3f32a29f43e0fe6915755718c2
Trevor Hartman <trevorhartman@gmail.com>
2013-05-26 17:41:51 -0600

    Require campfire access token when performing commands over API

--- src/yetibot/webapp/server.clj
+++ src/yetibot/webapp/server.clj
@@ -6,17 +6,20 @@
     [compojure.response :as response]
     [hiccup.core :refer :all]
     [yetibot.core :refer [direct-cmd]]
+    [yetibot.campfire :refer [chat-data-structure self]]
     [compojure.core :refer :all]))
 
-(defn api [command]
-  (let [res (direct-cmd command nil)]
-    (yetibot.campfire/chat-data-structure res)
-    res))
+(defn api [{:keys [command token]}]
+  (if (empty? token)
+    "Please provide a Campfire access token"
+    (if-let [user (:user (yetibot.campfire/self token))]
+      (-> (direct-cmd command user) yetibot.campfire/chat-data-structure)
+      "Invalid user access token")))
 
 (defroutes app-routes
   (GET "/" [] (views/layout))
-  (GET "/api" [command] (api command))
-  (POST "/api" [command] (api command))
+  (GET "/api" [& params] (api params))
+  (POST "/api" [& params] (api params))
   (route/resources "/"))
 
 (def app (handler/site app-routes))
