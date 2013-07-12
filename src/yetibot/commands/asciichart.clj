4c8cba0a5fb53de9dbdd206b988b95564fbe7562
Trevor Hartman <trevorhartman@gmail.com>
2013-07-12 09:20:41 -0600

    Pass parsed map to fix asciichart padding

--- src/yetibot/commands/asciichart.clj
+++ src/yetibot/commands/asciichart.clj
@@ -13,13 +13,15 @@
 
 (defn max-label [m] (apply max (map (fn [[k v]] (count (str k))) m)))
 
+
+
 (defn asciichart-cmd
   [{items :opts}]
   "asciichart <collection> # outputs a simple ascii chart. <collection> may either be
    a list of numbers, or a map whose vals are numbers."
   ; normalize items into a nested vector to be chartified
   (let [parsed-map (or (split-kvs items) (map vector items items))
-        chartify-padded (partial chartify (max-label items))]
+        chartify-padded (partial chartify (max-label parsed-map))]
     (map chartify-padded parsed-map)))
 
 (cmd-hook #"asciichart"
