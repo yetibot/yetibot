fb9af2748e5c5f1cd4e54353ad03f7f4cc2577d3
Trevor Hartman <trevorhartman@gmail.com>
2013-07-16 20:53:41 -0600

    Add buffer cmd; fix #201

---
+++ src/yetibot/commands/buffer.clj
@@ -0,0 +1,29 @@
+(ns yetibot.commands.buffer
+  (:require [yetibot.hooks :refer [cmd-hook cmd-unhook]]))
+
+(defonce buffer (atom []))
+
+(defn show-buffer []
+  (let [b @buffer]
+    (if (empty? b) "The buffer is empty" b)))
+
+(defn buffer-cmd
+  "buffer # accumulate arguments or piped contents into a buffer"
+  [{:keys [args opts]}]
+  (when (coll? opts) (swap! buffer (partial reduce conj) opts))
+  (swap! buffer conj args))
+
+(defn flush-buffer
+  "buffer flush # flush the buffer, outputting its contents"
+  [_] (let [b (show-buffer)]
+        (reset! buffer [])
+        b))
+
+(defn peek-buffer
+  "buffer peek # peek at the buffer's contents without flushing"
+  [_] (show-buffer))
+
+(cmd-hook ["buffer" #"^buffer$"]
+          #"peek" peek-buffer
+          #"flush" flush-buffer
+          _ buffer-cmd)
