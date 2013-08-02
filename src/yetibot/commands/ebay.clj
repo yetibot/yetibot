6f7215c42e5cb99874b3a83aab5c6c2244834880
Trevor Hartman <trevorhartman@gmail.com>
2013-08-02 10:52:55 -0600

    Combine price and title in ebay listings format

--- src/yetibot/commands/ebay.clj
+++ src/yetibot/commands/ebay.clj
@@ -2,14 +2,18 @@
   (:require [yetibot.api.ebay :refer [find-item]]
             [yetibot.hooks :refer [cmd-hook]]))
 
+(defn price-and-title [json]
+  (let [price ((comp (partial str "$") :__value__ first :currentPrice first :sellingStatus) json)
+        title ((comp first :title) json)]
+    (format "%s: %s" price title)))
+
 (defn format-listings [json]
   (let [items (-> json :findItemsByKeywordsResponse first :searchResult first :item)]
     (interleave
       (map (comp flatten
-                 (juxt :title
-                       (comp (partial str "$") :__value__ first :currentPrice first :sellingStatus)
-                       :galleryURL
-                       :viewItemURL))
+                 (juxt price-and-title
+                       :viewItemURL
+                       :galleryURL))
            (take 10 items))
       (repeat ["--"]))))
 
