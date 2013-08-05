6ba8c1e3d56c0e43054c85a96922de8c5eed74ff
Trevor Hartman <trevorhartman@gmail.com>
2013-08-05 12:21:25 -0600

    Support nested backticked sub-expressions

--- src/yetibot/parser.clj
+++ src/yetibot/parser.clj
@@ -7,12 +7,25 @@
   (insta/parser
     "expr = cmd <space> (<pipe> <space> cmd)+ | cmd
      cmd = <' '>* space-separated-words <' '>*
+     sub-expr = <backtick> (expr | sub-expr) <backtick>
 
      <space-separated-words> = word (<space> word)*
-     <word> = #'[^ |]+'
+     <word> = sub-expr | #'[^` |]+'
      <space> = #'[ ]+'
      <pipe> = #'[|]'
+     <backtick> = #'`'
      "))
 
+(parser "echo `echo `catfact` `catfact``")
+[:expr [:cmd "echo" [:sub-expr [:expr [:cmd "echo" [:sub-expr [:expr [:cmd "catfact"]]] [:sub-expr [:expr [:cmd "catfact"]]]]]]]]
+
+(parser "`catfact`")
+[:expr [:cmd "hello" [:sub-expr [:expr [:cmd "foo"]]] "there"]]
+
+[:expr
+ [:cmd "hello"
+  [:sub-expr [:backtick "`"] [:expr [:cmd "foo"]] [:backtick "`"]]
+  "there"]]
+
 (parser "hello there")
-(parser "hello there | 123 | how are you")
+[:expr [:cmd "hello" "there"] [:cmd "123"] [:cmd "how" "are" "you"]]
