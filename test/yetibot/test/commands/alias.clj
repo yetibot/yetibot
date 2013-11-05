12a68bfda380423b1954980bfdfe9662fd65286a
Trevor Hartman <trevorhartman@gmail.com>
2013-11-04 17:26:45 -0700

    Kill db connection in tests

--- test/yetibot/test/commands/alias.clj
+++ test/yetibot/test/commands/alias.clj
@@ -1,27 +1,9 @@
 (ns yetibot.test.commands.alias
   (:require
-    [yetibot.db :as db]
-    [yetibot.util :refer [with-fresh-db]]
     [clojure.test :refer :all]
     [clojure.string :refer [split]]
     [yetibot.commands.alias :refer :all]))
 
-(def user {:id "foobar"})
-
-(defn start-db [f]
-  (db/start)
-  (f))
-
-(use-fixtures :once start-db)
-
-(deftest split-alias-with-spaces
-  (split "can we do stuff" #" " 2)
-  (split "can" #" " 2)
-  )
-
-(deftest test-add-alias
-  (with-fresh-db
-    (let [args ["a = random \\| echo hi"
-                "b = echo hi"
-                "c = random \\| echo http://foo.com?bust=%s"]]
-      (dorun (map #(add-alias {:user user :args %}) args)))))
+(let [args ["a = random \\| echo hi"
+            "b = echo hi"
+            "c = random \\| echo http://foo.com?bust=%s"]])
