23c1816ddd9ea2f93c8ecdf049fb6378afa85d79
Trevor Hartman <trevorhartman@gmail.com>
2013-11-11 16:09:29 -0700

    Use new config in JIRA

--- src/yetibot/api/jira.clj
+++ src/yetibot/api/jira.clj
@@ -1,11 +1,15 @@
 (ns yetibot.api.jira
-  (:use [yetibot.util.http]))
+  (:require
+    [yetibot.config :refer [config-for-ns conf-valid?]]
+    [yetibot.util.http :refer [get-json]]))
 
-(def domain (System/getenv "JIRA_DOMAIN"))
+(def config (config-for-ns))
+(def configured? (conf-valid?))
+
+(def domain (:domain config))
 (def base-uri (str "https://" domain))
 (def api-uri (str base-uri "/rest/api/latest"))
-(def auth {:user (System/getenv "JIRA_USER")
-           :password (System/getenv "JIRA_PASSWORD")})
+(def auth (select-keys config [:user :password]))
 
 (defn get-issue
   "Fetch json for a given JIRA"
--- src/yetibot/observers/jira.clj
+++ src/yetibot/observers/jira.clj
@@ -1,30 +1,31 @@
 (ns yetibot.observers.jira
   (:require [yetibot.api.jira :as jira]
-            [clojure.string :as s])
-  (:use [yetibot.util]
-        [yetibot.hooks :only [obs-hook]]
-        [yetibot.util.http]
-        [yetibot.chat :only (chat-data-structure)]
-        [clojure.contrib.cond]))
+            [clojure.string :as s]
+            [taoensso.timbre :refer [info warn error]]
+            [yetibot.hooks :refer [obs-hook]]
+            [yetibot.chat :refer [chat-data-structure]]))
 
-(def project-keys
-  (s/split (str (System/getenv "JIRA_PROJECT_KEYS")) #","))
+(if jira/configured?
+  (do
+    (def project-keys (:project-keys jira/config))
 
-; build a regex pattern to match jira issues
-(def issue-pattern
-  (re-pattern
-    (str "(" (s/join "|" project-keys) ")" "-\\d+")))
+    ; build a regex pattern to match jira issues
+    (def issue-pattern
+      (re-pattern
+        (str "(" (s/join "|" project-keys) ")" "-\\d+")))
 
-(defn report-jira [issue]
-  (let [ji (jira/get-issue issue)
-        fs (:fields ji)]
-    (chat-data-structure [(-> fs :summary)
-                  (str "Assignee: " (-> fs :assignee :displayName))
-                  (str "Status: " (-> fs :status :name))
-                  (str jira/base-uri "/browse/" issue)])))
+    (defn report-jira [issue]
+      (let [ji (jira/get-issue issue)
+            fs (:fields ji)]
+        (chat-data-structure [(-> fs :summary)
+                              (str "Assignee: " (-> fs :assignee :displayName))
+                              (str "Status: " (-> fs :status :name))
+                              (str jira/base-uri "/browse/" issue)])))
 
-(obs-hook
-  #{:message}
-  (fn [event-json]
-    (if-let [is (set (map first (re-seq issue-pattern (:body event-json))))]
-      (doall (map report-jira is)))))
+    (obs-hook
+      #{:message}
+      (fn [event-json]
+        (prn "obs " event-json)
+        (if-let [is (set (map first (re-seq issue-pattern (:body event-json))))]
+          (doall (map report-jira is))))))
+  (info "JIRA is not configured"))
