e8cea8e53d63eaed8ac607fef58d4960ac69b96d
Trevor Hartman <trevorhartman@gmail.com>
2013-11-11 13:43:42 -0700

    Use new config for ebay

--- src/yetibot/api/ebay.clj
+++ src/yetibot/api/ebay.clj
@@ -1,15 +1,16 @@
 (ns yetibot.api.ebay
-  (:require [yetibot.util :refer [env]]
-            [yetibot.util.http :refer [encode get-json]]))
+  (:require
+    [yetibot.config :refer [config-for-ns]]
+    [yetibot.util.http :refer [encode get-json]]))
 
-(def config {:appid (:EBAY_APPID env)})
+(def config (config-for-ns))
 
 (def endpoint "http://svcs.ebay.com/services/search/FindingService/v1")
 
 (defn find-item-endpoint [term]
   (format "%s?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=%s&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=%s"
           endpoint
-          (:appid config)
+          (:app-id config)
           (encode term)))
 
 (def find-item (comp get-json find-item-endpoint))
