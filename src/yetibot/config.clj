e7c74ed6b3663374eaff447fe7b81338bd7fdbb4
Trevor Hartman <trevorhartman@gmail.com>
2013-11-11 11:51:38 -0700

    Add config-for-ns to pull the corresponding config for current *ns*

--- src/yetibot/config.clj
+++ src/yetibot/config.clj
@@ -1,8 +1,10 @@
 (ns yetibot.config
+  "Config is stored in an edn file. The config data structure maps to the
+   namespaces of the code that depends on the config."
   (:require
     [taoensso.timbre :refer [info warn error]]
     [clojure.edn :as edn]
-    [clojure.string :refer [blank?]]))
+    [clojure.string :refer [blank? split]]))
 
 (def ^:private config-path "config/config.edn")
 
@@ -14,15 +16,20 @@
 (defn reload-config []
   (info "☐ Loading config")
   (reset! config (load-edn config-path))
+  (info (load-edn config-path))
   (info "☑ Config loaded"))
 
 (defn get-config
   [path]
   (let [path (if (coll? path) path [path])]
-    (get-in @config (into [:yetibot] path))))
+    (get-in @config path)))
 
-(defn conf-valid? [c]
-  (every? (complement (comp blank? str)) (vals c)))
+(defn config-for-ns []
+  (get-config (map keyword (split (str *ns*) #"\."))))
+
+(defn conf-valid?
+  ([] (conf-valid? (config-for-ns)))
+  ([c] (every? (complement (comp blank? str)) (vals c))))
 
 (defn start []
   (reload-config))
