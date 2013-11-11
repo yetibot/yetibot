4165ff8f026bdbea6d624379f36dcb65a5323c4e
Trevor Hartman <trevorhartman@gmail.com>
2013-11-11 11:51:51 -0700

    Use new config in db

--- src/yetibot/db.clj
+++ src/yetibot/db.clj
@@ -1,10 +1,10 @@
 (ns yetibot.db
   (:require
+    [yetibot.config :as config :refer [config-for-ns conf-valid?]]
     [datomico.db :as db]
     [datomico.core :as dc]
     [datomic.api :as api]
-    [taoensso.timbre :refer [info warn error]]
-    [yetibot.util :refer [env]]))
+    [taoensso.timbre :refer [info warn error]]))
 
 ; TODO: loop through namespaces looking for a "schema" to load
 ; (do it manually for now)
@@ -18,7 +18,10 @@
   (for [n nss] (do (require n) (deref (ns-resolve n 'schema)))))
 
 (defn start []
-  (info "☐ Loading Datomic schemas")
-  (dc/start {:uri (:YETIBOT_DATOMIC_URL env)
-             :schemas schemas})
-  (info "☑ Datomic connected"))
+  (if (conf-valid?)
+    (do
+      (info "☐ Loading Datomic schemas")
+      (dc/start {:uri (:datomic-url (config-for-ns))
+                 :schemas schemas})
+      (info "☑ Datomic connected"))
+    (warn ":datomic-url is not configured, unable to connect.")))
