fe36d597bffde1bf5e6fc9bf6dc206873dd75186
Trevor Hartman <trevorhartman@gmail.com>
2013-11-12 20:47:16 -0700

    Use new config in weather

--- src/yetibot/commands/weather.clj
+++ src/yetibot/commands/weather.clj
@@ -1,16 +1,16 @@
 (ns yetibot.commands.weather
   (:require [yetibot.util.http :refer [get-json]]
-            [yetibot.util :refer [env]]
+            [yetibot.config :refer [config-for-ns conf-valid?]]
             [yetibot.hooks :refer [cmd-hook]]))
 
-(def api-key (:WUNDERGROUND_API_KEY env))
-(def default-loc (:WEATHER_DEFAULT_LOCATION env))
+(def config (config-for-ns))
+
+(def api-key (:wunderground-api-key config))
+(def default-zip (:default-zip config))
 
 (defn- conditions [loc]
   (get-json (format "http://api.wunderground.com/api/%s/conditions/q/%s.json"
                     api-key loc)))
-
-(defn prn-keys [coll ks] (map (fn [k] (str k ": " (k coll) "\n")) ks))
 
 (defn- format-conditions [c]
   (let [co (:current_observation c)
@@ -31,7 +31,7 @@
 
 (defn default-weather-cmd
   "weather # look up weather for default location"
-  [_] (weather-cmd {:match default-loc}))
+  [_] (weather-cmd {:match default-zip}))
 
 (cmd-hook #"weather"
           #".+" weather-cmd
