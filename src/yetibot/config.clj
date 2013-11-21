641833ce5345c96f015d372513f8856b21fbf187
Trevor Hartman <trevorhartman@gmail.com>
2013-11-21 12:15:58 -0700

    Enable updating and writing config to disk

--- src/yetibot/config.clj
+++ src/yetibot/config.clj
@@ -3,6 +3,7 @@
    namespaces of the code that depends on the config."
   (:require
     [taoensso.timbre :refer [info warn error]]
+    [clojure.pprint :refer [pprint]]
     [clojure.edn :as edn]
     [clojure.string :refer [blank? split]]))
 
@@ -15,7 +16,7 @@
     (edn/read-string (slurp path))
     (catch Exception _
       (warn "Config is missing")
-      {})))
+      {:yetibot {}})))
 
 (defn reload-config []
   (let [new-conf (load-edn config-path)]
@@ -28,6 +29,17 @@
   [& path]
   (let [path (if (coll? path) path [path])]
     (get-in @config path)))
+
+(defn write-config []
+  (spit config-path (with-out-str (pprint @config))))
+
+(defn update-config
+  "Update the config data structure and write it to disk."
+  [& path-and-val]
+  (let [path (butlast path-and-val)
+        value (last path-and-val)]
+    (swap! config update-in path (constantly value))
+    (write-config)))
 
 (defn config-for-ns []
   (apply get-config (map keyword (split (str *ns*) #"\."))))
