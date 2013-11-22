218a0a57fea5994c90d2c40902414ca74f20a79c
Trevor Hartman <trevorhartman@gmail.com>
2013-11-21 17:32:24 -0700

    Support multiple Jenkins instances

--- src/yetibot/commands/jenkins.clj
+++ src/yetibot/commands/jenkins.clj
@@ -1,127 +1,46 @@
 (ns yetibot.commands.jenkins
-  (:require [http.async.client :as client]
-            [clojure.data.json :as json]
-            [clojure.string :as s]
-            [robert.hooke :as rh]
-            [clj-time.core :as t]
-            [clj-time.coerce :as c]
-            [clojure.core.cache :as cache]
-            [yetibot.config :refer [config-for-ns]]
-            [yetibot.hooks :refer [cmd-hook]]
-            [yetibot.util.http :refer [fetch get-json with-client]]))
+  (:require
+    [yetibot.models.jenkins :as model]
+    [clojure.string :as s]
+    [yetibot.hooks :refer [cmd-hook]]))
 
-
-(def config (config-for-ns))
-
-(def base-uri (:uri config))
-(def auth {:user (:user config)
-           :password (:api-key config)
-           :preemptive true})
-(def default-job (:default-job config))
-
-; Helpers
-(def cache-ttl (* 1000 60 60))
-(def jenkins-cache (atom (cache/ttl-cache-factory {} :ttl cache-ttl)))
-
-(defn fetch-root []
-  (let [uri (format "%s/api/json" base-uri)]
-    (get-json uri auth)))
-
-(defn root-data []
-  (swap! jenkins-cache
-         (fn [c]
-           (if (cache/has? c :root)
-             (cache/hit c :root)
-             (cache/miss c :root (fetch-root))))))
-
-(defn job-names [] (map :name (:jobs (:root (root-data)))))
-
-
-
-; API Calls
-(defn status [job-name]
-  (with-open [client (client/create-client)]
-    (let [uri (str base-uri "job/" job-name "/lastBuild/api/json")
-          response (client/GET client uri :auth auth)]
-      (client/await response)
-      (let [unparsed (client/string response)]
-        (json/read-json unparsed)))))
-
-(defn job-status [job-name]
-  "Sends job status info to chat. Sample output:
-  SUCCESS at Wed Nov 16. Started by trevor.
-  Currently building? false
-  [Changeset]"
-  (let [json (status job-name)]
-    (if-let [building (str (:building json))] ; convert to string so a `false` doesn't give a false-negative
-      (let [result (str (:result json))
-                   changeset (s/join
-                               \newline
-                               (map (fn [i]
-                                      (let [msg (:msg i)
-                                            author (if (seq (:author i))
-                                                     (:fullName (:author i)) ; git
-                                                     (:user i))]; svn
-                                        (str author ": " msg)))
-                                    (:items (:changeSet json))))]
-        [(:url json)
-         (str
-           (if result (str result " at "))
-           (c/to-date (c/from-long (:timestamp json)))
-           ". "
-           (:shortDescription (first (:causes (first (:actions json)))))
-           ".")
-         (str "Currently building: " building)
-         ""
-         changeset]))))
-
-(defn report-job-url [job-name]
-  ; Wait a second before requesting the job url so that Jenkins has time to actually
-  ; start the build. Otherwise, the previous build url will be reported instead of
-  ; the latest.
-  (Thread/sleep 8000)
-  (let [json (status job-name)]
-    (yetibot.chat/chat-data-structure (:url json))))
+(defn find-job-and-do [job-to-match f]
+  (if-let [job (model/resolve-job-info job-to-match)]
+    (f job)
+    (format "I couldn't match any jobs on %s." job-to-match)))
 
 (defn build
   "jen build <job-name>"
   [{[_ job-pattern] :match}]
-  (let [job-pattern (re-pattern job-pattern)]
-    (letfn [(match-job [pattern job] (when (re-find pattern job) job))]
-      (if-let [job-name (some (partial match-job job-pattern) (job-names))]
-        (let [uri (format "%sjob/%s/build" base-uri job-name)
-                  response (fetch uri auth)]
-          (future (report-job-url job-name))
-          (str "Starting build on " job-name))
-        (format "I couldn't match any jobs on  %s. Get it right next time." job-pattern)))))
+  (find-job-and-do job-pattern model/build-job))
 
 (defn list-jobs
-  ([] (job-names))
-  ([n] (take n (job-names))))
+  ([] (model/job-names))
+  ([n] (take n (model/job-names))))
 
 (defn list-jobs-matching [match]
   (let [p (re-pattern (s/trim match))]
-    (filter #(re-find p %) (job-names))))
+    (filter #(re-find p %) (model/job-names))))
 
 (defn build-default-cmd
   "jen build # build default job if configured"
-  [_] (if default-job
-        (build {:match [nil default-job]})
+  [_] (if-let [job-pair (model/default-job)]
+        (model/build-job job-pair)
         "No default job configured"))
 
 (defn status-cmd
-  "
-jen status <job-name>       # show Jenkins status for <job-name>
-jen status                  # show Jenkins status for default job if configured"
+  "jen status <job-name>       # show Jenkins status for <job-name>
+   jen status                  # show Jenkins status for default job if configured"
   [{args :match}]
   (if (coll? args)
-    (job-status (second args))
-    (job-status default-job)))
+    (find-job-and-do (second args) model/job-status)
+    (if-let [df (model/default-job)]
+      (model/job-status df)
+      "No default job configured")))
 
 (defn list-cmd
-  "
-jen list                    # lists all jenkins jobs
-jen list <pattern>          # lists jenkins jobs containing <string>"
+  "jen list                    # lists all jenkins jobs
+   jen list <pattern>          # lists jenkins jobs containing <string>"
   [{args :match}]
   (if (coll? args)
     (list-jobs-matching (second args))
---
+++ src/yetibot/models/jenkins.clj
@@ -0,0 +1,135 @@
+(ns yetibot.models.jenkins
+  (:require
+    [yetibot.util.http :refer [get-json fetch]]
+    [clojure.string :as s]
+    [clj-time.coerce :as c]
+    [yetibot.config :refer [get-config conf-valid?]]
+    [clojure.core.memoize :as memo]))
+
+(defonce instance-root-data (atom {}))
+
+(def config (partial get-config :yetibot :models :jenkins))
+(defn configured? [] (conf-valid? (config)))
+(def cache-ttl (get-config :yetibot :models :jenkins :cache-ttl))
+
+; Helpers
+
+(defn- instances [] (-> (config) :instances))
+(defn- auth-for [instance]
+  {:user (:user instance)
+   :password (:api-key instance)
+   :preemptive true})
+
+(defn setup-instance-pairs
+  "If instance name key doesn't already exist, assoc it with its value set to
+   a ttl-memoized function that fetches itself."
+  []
+  (doseq [[inst-name inst] (instances)]
+    (when-not (inst-name @instance-root-data)
+      (swap!
+        instance-root-data
+        assoc
+        inst-name
+        {:config inst
+         :fetcher (-> (fn []
+                        (let [uri (format "%s/api/json" (:uri inst))]
+                          (get-json uri (auth-for inst))))
+                      (memo/ttl :ttl/threshold cache-ttl))}))))
+
+(defn prime-memos
+  "Executes all the fetcher functions in parallel in order to prime their memo
+   caches."
+  []
+  (setup-instance-pairs)
+  (doall
+    (pmap
+      (fn [[inst-name inst]]
+        (when-let [instance-info (inst-name @instance-root-data)]
+          ((:fetcher instance-info))))
+      (instances))))
+
+(prime-memos)
+
+; Getters
+
+(defn jobs-to-info []
+  (->> (map (fn [[inst-key inst-info]]
+              (let [data ((:fetcher inst-info))]
+                (map (fn [job]
+                       {(:name job) (merge {:job-name (:name job)} inst-info)})
+                     (:jobs data))))
+            @instance-root-data)
+       flatten
+       (reduce conj)))
+
+(defn job-names []
+  (keys (jobs-to-info)))
+
+(defn resolve-job-info [job-to-match]
+  (let [p (re-pattern job-to-match)
+        matches (filter (fn [[job-name inst-info]] (re-find p job-name))
+                        (jobs-to-info))]
+    (if (empty? matches)
+      nil
+      (first matches))))
+
+(defn default-job []
+  (when-let [[_ inst-info] (first (filter (fn [[inst-k inst-info]]
+                                            (let [d (-> inst-info :config :default-job)]
+                                              (not (s/blank? d))))
+                                          @instance-root-data))]
+    (resolve-job-info (-> inst-info :config :default-job))))
+
+; API calls
+
+(defn status [[job-name job-info]]
+  (let [base-uri (-> job-info :config :uri)
+        uri (str base-uri "job/" job-name "/lastBuild/api/json")]
+    (prn "get status from uri" uri)
+    (get-json uri (auth-for (:config job-info)))))
+
+(defn job-status [[job-name job-info]]
+  "Sends job status info to chat. Sample output:
+   SUCCESS at Wed Nov 16. Started by trevor.
+   Currently building? false
+   [Changeset]"
+  (try
+    (let [json (status [job-name job-info])]
+      (if-let [building (str (:building json))] ; convert to string so a `false` doesn't give a false-negative
+        (let [result (str (:result json))
+              changeset (s/join
+                          \newline
+                          (map (fn [i]
+                                 (let [msg (:msg i)
+                                       author (if (seq (:author i))
+                                                (:fullName (:author i)) ; git
+                                                (:user i))]; svn
+                                   (str author ": " msg)))
+                               (:items (:changeSet json))))]
+          [(:url json)
+           (str
+             (if result (str result " at "))
+             (c/to-date (c/from-long (:timestamp json)))
+             ". "
+             (:shortDescription (first (:causes (first (:actions json)))))
+             ".")
+           (str "Currently building: " building)
+           ""
+           changeset])))
+    (catch Exception _
+      (format "Couldn't fetch status for %s. Perhaps it has no build history?" job-name))))
+
+(defn report-job-url [[job-name job-info]]
+  ; Wait a few seconds before requesting the job url so that Jenkins has time to
+  ; actually start the build. Otherwise, the previous build url will be reported
+  ; instead of the latest.
+  (Thread/sleep 8000)
+  (let [json (status [job-name job-info])]
+    (yetibot.chat/chat-data-structure (:url json))))
+
+(defn build-job [[job-name job-info]]
+  (let [base-uri (-> job-info :config :uri)
+        uri (format "%sjob/%s/build" base-uri job-name)
+        response (fetch uri (auth-for (:config job-info)))]
+    (future (report-job-url [job-name job-info]))
+    (str "Starting build on " job-name)))
