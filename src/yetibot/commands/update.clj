25d2cabd8d5654e2951a899ad98fd3ce4dd81278
Trevor Hartman <trevorhartman@gmail.com>
2013-11-25 09:23:47 -0700

    Improve error handling around git pull in update

--- src/yetibot/commands/update.clj
+++ src/yetibot/commands/update.clj
@@ -1,22 +1,35 @@
 (ns yetibot.commands.update
-  (:require [clojure.java.shell :as shell]
-            [clojure.string :as s]
-            [yetibot.hooks :refer [cmd-hook]]))
+  (:require
+    [taoensso.timbre :refer [info warn error]]
+    [clojure.java.shell :as shell]
+    [clojure.string :as s]
+    [yetibot.hooks :refer [cmd-hook]]))
 
 (def gpr "git pull --rebase")
 (def log "git log --oneline @{1}..")
 (def up-to-date-msg "Current branch master is up to date.\n")
+(def default-err-msg "Error in git pull, try again")
 
 (defn sh [c]
   (apply shell/sh (s/split c #"\s")))
 
+(defn- handle-err [pull]
+  (if (s/blank? (:err pull))
+    default-err-msg
+    (:err pull)))
+
+(defn- handle-succ [pull]
+  (if (= up-to-date-msg (:out pull))
+    up-to-date-msg
+    (:out (sh log))))
+
 (defn update-cmd
   "update # update YetiBot's local git repo"
   [_]
-  (let [pull (sh gpr)]
-    (if (= up-to-date-msg (:out pull))
-      up-to-date-msg
-      (:out (sh log)))))
+  (let [pull (sh gpr)
+        err? (= 1 (:exit pull))]
+    (info pull)
+    ((if err? handle-err handle-succ) pull)))
 
 (cmd-hook #"update"
           _ update-cmd)
