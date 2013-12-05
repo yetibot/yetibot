14da83e9e85b49600fb538a9f14a9837ff9e3f0b
Trevor Hartman <trevorhartman@gmail.com>
2013-12-05 15:28:36 -0700

    Preserve whitespace during parsing; fix #274; fix #241

--- src/yetibot/parser.clj
+++ src/yetibot/parser.clj
@@ -1,5 +1,6 @@
 (ns yetibot.parser
   (:require
+    [clojure.core.match :refer [match]]
     [yetibot.interpreter :refer [handle-expr]]
     [clojure.string :refer [join]]
     [instaparse.core :as insta]))
@@ -17,11 +18,11 @@
      cmd = words
      <sub-expr> = <backtick> expr <backtick> | nestable-sub-expr
      <nestable-sub-expr> = <dollar> <lparen> expr <rparen>
-     words = word (<space>* word)*
+     words = word (space* word)*
      <word> = sub-expr | parened | word-chars
      <word-chars> = #'[^ `$()|]+'
      parened = lparen words rparen
-     <space> = #'[ ]+'
+     space = ' '
      <pipe> = #'[|]'
      <dollar> = <'$'>
      <lparen> = '('
@@ -31,27 +32,11 @@
 (def transformer
   (partial
     insta/transform
-    {:words (fn [& args] (join " " args))
+    {:words (fn [& words] (join words))
+     :space str
      :parened str
      :cmd identity
      :expr handle-expr}))
 
-
-;; There is a bug in parsing/evaluating. In an expression like:
-;; "echo $(echo foo)bar" the result should be "foobar" but it's actually "foo bar".
-;; This is because when the AST is evaluated the inner "echo foo" is a separate :expr
-;; and the string "bar" follows it, siblings in a :words leaf. Possible solution:
-;;
-;; - multi-pass evaluation where sub exprs get evaluated to another leaf type, like
-;; [:words [:evaluated "foo"] "bar"]. The :words flattener would concat "foo" and
-;; "bar" without the space. The problem with that is that is then all sub-exprs would
-;; lack a space. The problem is the original lack of space information is not
-;; preserved.
-;;
-;; How do we preserve it? Do we abandon parsing :words?
-;;
-;; Maybe it should only be split into [first & rest] words since the first is the
-;; only one that really matters (i.e. it's the cmd).
-;;
 (defn parse-and-eval [input]
   (-> input parser transformer))
--- test/yetibot/test/parser.clj
+++ test/yetibot/test/parser.clj
@@ -6,71 +6,49 @@
 (deftest single-cmd-test
   (is (= (parser "uptime")
          [:expr [:cmd [:words "uptime"]]]))
-  (is (= (parser "echo qux") [:expr [:cmd [:words "echo" "qux"]]])
+  (is (= (parser "echo qux")
+         [:expr [:cmd [:words "echo" [:space " "] "qux"]]])
       "Single commands should be parsed"))
 
 (deftest neighboring-sub-exprs
   (is (= (parser "echo $(echo foo)bar")
-         [:expr [:cmd [:words "echo" [:expr [:cmd [:words "echo" "foo"]]] "bar"]]])))
+         [:expr [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "echo" [:space " "] "foo"]]] "bar"]]])))
 
 (deftest piped-cmd-test
   (is
     (= (parser "echo hello | echo bar")
-       [:expr [:cmd [:words "echo" "hello"]] [:cmd [:words "echo" "bar"]]])
+       [:expr [:cmd [:words "echo" [:space " "] "hello"]] [:cmd [:words "echo" [:space " "] "bar"]]])
     "Piped commands should be parsed"))
 
 (deftest sub-expr-test
   (is
     (=
      (parser "echo `catfact` | echo It is known:")
-     [:expr
-      [:cmd [:words "echo" [:expr [:cmd [:words "catfact"]]]]]
-      [:cmd [:words "echo" "It" "is" "known:"]]])
+     [:expr [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "catfact"]]]]] [:cmd [:words "echo" [:space " "] "It" [:space " "] "is" [:space " "] "known:"]]])
     "Backtick sub-expressions should be parsed")
   (is
     (=
      (parser "echo $(random)")
-     [:expr [:cmd [:words "echo" [:expr [:cmd [:words "random"]]]]]])
+     [:expr [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "random"]]]]]])
     "Standard sub-expressions should be parsed"))
 
 (deftest nested-sub-expr-test
   (is
     (=
      (parser "random | buffer | echo `number $(buffer | peek)`")
-     [:expr [:cmd [:words "random"]] [:cmd [:words "buffer"]] [:cmd [:words "echo" [:expr [:cmd [:words "number" [:expr [:cmd [:words "buffer"]] [:cmd [:words "peek"]]]]]]]]])
+     [:expr [:cmd [:words "random"]] [:cmd [:words "buffer"]] [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "number" [:space " "] [:expr [:cmd [:words "buffer"]] [:cmd [:words "peek"]]]]]]]]])
     "Nested sub-expressions should be parsed")
+  (is
+    (=
+     (parser
+       "echo foo
+        $(echo bar)")
+     [:expr [:cmd [:words "echo" [:space " "] "foo\n" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:expr [:cmd [:words "echo" [:space " "] "bar"]]]]]])
+    "Expressions with newlines should preserve the newline")
   (is
     (=
      (parser
        "urban random | buffer | echo `meme wizard: what is $(buffer peek | head)?`
         `meme chemistry: a $(buffer peek | head) is $(buffer peek | head 2 | tail)`")
-     [:expr
-      [:cmd [:words "urban" "random"]]
-      [:cmd [:words "buffer"]]
-      [:cmd
-       [:words
-        "echo"
-        [:expr
-         [:cmd
-          [:words
-           "meme"
-           "wizard:"
-           "what"
-           "is"
-           [:expr [:cmd [:words "buffer" "peek"]] [:cmd [:words "head"]]]
-           "?"]]]
-        "\n"
-        [:expr
-         [:cmd
-          [:words
-           "meme"
-           "chemistry:"
-           "a"
-           [:expr [:cmd [:words "buffer" "peek"]] [:cmd [:words "head"]]]
-           "is"
-           [:expr
-            [:cmd [:words "buffer" "peek"]]
-            [:cmd [:words "head" "2"]]
-            [:cmd [:words "tail"]]]]]]]]]
-     )
+     [:expr [:cmd [:words "urban" [:space " "] "random"]] [:cmd [:words "buffer"]] [:cmd [:words "echo" [:space " "] [:expr [:cmd [:words "meme" [:space " "] "wizard:" [:space " "] "what" [:space " "] "is" [:space " "] [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head"]]] "?"]]] "\n" [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:space " "] [:expr [:cmd [:words "meme" [:space " "] "chemistry:" [:space " "] "a" [:space " "] [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head"]]] [:space " "] "is" [:space " "] [:expr [:cmd [:words "buffer" [:space " "] "peek"]] [:cmd [:words "head" [:space " "] "2"]] [:cmd [:words "tail"]]]]]]]]])
     "Complex nested sub-expressions with newlines should be parsed"))
