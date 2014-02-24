d1eb8de5c5ee02c4e6fa350af7f7e94aca656e57
Trevor Hartman <trevorhartman@gmail.com>
2014-02-24 03:23:02 -0700

    Use yetibot.core 0.1.5

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  :welcome (println "Welcome to the YetiBot development repl!")}
   :jvm-opts ["-server" "-Xmx2G"]
   :dependencies [[org.clojure/clojure "1.5.1"],
-                 [yetibot.core "0.1.4"]
+                 [yetibot.core "0.1.5"]
 
                  ; apis
                  [tentacles "0.2.5"]
