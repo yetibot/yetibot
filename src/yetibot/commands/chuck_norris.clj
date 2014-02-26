52ed0d483615aa00661bd5df9098746fe9a81c7b
Trevor Hartman <trevorhartman@gmail.com>
2014-02-25 17:23:42 -0700

    Limit chuck to nerdy category; fix #322

--- src/yetibot/commands/chuck_norris.clj
+++ src/yetibot/commands/chuck_norris.clj
@@ -3,7 +3,7 @@
     [yetibot.core.hooks :refer [cmd-hook cmd-unhook]]
     [yetibot.core.util.http :refer [get-json]]))
 
-(def endpoint "http://api.icndb.com/jokes/random")
+(def endpoint "http://api.icndb.com/jokes/random?limitTo=[nerdy]")
 
 (defn chuck-joke
   "chuck # tell a random Chuck Norris joke"
