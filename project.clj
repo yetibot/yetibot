685385d70a64612db482cdfc573f2bf6bfef5e1e
Trevor Hartman <trevorhartman@gmail.com>
2014-02-25 17:30:11 -0700

    lein-release plugin: bumped version from 0.1.15 to 0.1.16-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot "0.1.15"
+(defproject yetibot "0.1.16-SNAPSHOT"
   :description "A command line in your chat, where chat ∈ {irc,campfire}."
   :url "https://github.com/devth/yetibot"
   :license {:name "Eclipse Public License"
