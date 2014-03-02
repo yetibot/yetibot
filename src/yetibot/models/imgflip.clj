f15d030b0b42a176dd92695a99d6cd3f802c36f3
Trevor Hartman <trevorhartman@gmail.com>
2014-03-02 09:37:37 -0700

    Fallback to scraping search for meme search; fix #281

--- src/yetibot/models/imgflip.clj
+++ src/yetibot/models/imgflip.clj
@@ -1,6 +1,8 @@
 (ns yetibot.models.imgflip
   (:require
+    [taoensso.timbre :refer [info warn error]]
     [clojure.string :as s :refer [split join]]
+    [clj-http.client :as client]
     [yetibot.core.config :refer [get-config conf-valid?]]
     [yetibot.core.util.http :refer [get-json map-to-query-string encode]]
     [clojure.core.memoize :as m]))
@@ -12,6 +14,8 @@
 (defn fetch-memes [] (get-json (str endpoint "/get_memes")))
 (defonce memes (m/ttl fetch-memes :ttl/threshold 3600000)) ; one hour memo
 
+(defn- parse-base-36-int [s] (Integer/parseInt s 36))
+
 (defn- match-meme-name
   "Remove spaces from meme name to provide great chance of match"
   [p meme]
@@ -19,13 +23,27 @@
     (re-find p (:name meme))
     (re-find p (s/replace (:name meme) #"\s" ""))))
 
+; todo: append results of search-via-scrape to cached (memes)
+(defn search-via-scrape [q]
+  (info "search via scraping" q)
+  (let [res (client/get "https://imgflip.com/memesearch" {:query-params {:q q}})]
+    (->> res
+         :body
+         (re-seq #"alt\=\"([^\"]+)\"\s+src\=\'.+imgflip\.com\/([\w\d]+).jpg\'")
+         (map (fn [[_ alt id]]
+                {:name (s/replace alt #"\sMeme Template( Thumbnail)*" "")
+                 :url (str "http://i.imgflip.com/" id ".jpg")
+                 :id (parse-base-36-int id)})))))
+
 (defn search-memes [query]
   (let [ms (-> (memes) :data :memes)
         p (re-pattern (str "(?i)" query))
         match-fn (partial match-meme-name p)]
     (let [matching-ms (filter match-fn ms)]
-      (when-not (empty? matching-ms)
-        matching-ms))))
+      (if-not (empty? matching-ms)
+        matching-ms
+        ; fallback to search-via-scrape if cached results don't contain a match
+        (search-via-scrape query)))))
 
 (defn generate-meme [id text0 text1]
   (get-json
@@ -46,3 +64,4 @@
                           (split-at (/ (count spl) 2) spl))))))
     {:success false
      :error_message (str "Couldn't find any memes for " query)}))
+
