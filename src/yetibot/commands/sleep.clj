84ef9cfce5f6c5ba4bedf204537096cc5a06f6f8
Trevor Hartman <trevorhartman@gmail.com>
2014-03-15 03:04:14 -0600

    Add sleep command; fix #306

---
+++ src/yetibot/commands/sleep.clj
@@ -0,0 +1,16 @@
+(ns yetibot.commands.sleep
+  (:require
+    [taoensso.timbre :refer [info warn error]]
+    [yetibot.core.hooks :refer [cmd-hook]]))
+
+(def max-sleep 10000)
+
+(defn sleep-cmd
+  "sleep <n> # sleep for <n> milliseconds up to 10,000"
+  [{:keys [match]}]
+  (let [wait (min max-sleep (read-string match))]
+    (Thread/sleep wait)
+    (format "Slept for %,d milliseconds" wait)))
+
+(cmd-hook ["sleep" #"^sleep$"]
+  #"\d+" sleep-cmd)
