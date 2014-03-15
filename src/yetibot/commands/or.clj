46b88ff9d26e09d3f39bf092f4c34c67c7f1f7f3
Trevor Hartman <trevorhartman@gmail.com>
2014-03-15 03:12:13 -0600

    Add or command; fix #316

---
+++ src/yetibot/commands/or.clj
@@ -0,0 +1,16 @@
+(ns yetibot.commands.or
+  (:require
+    [clojure.string :refer [blank?]]
+    [taoensso.timbre :refer [info warn error]]
+    [yetibot.core.hooks :refer [cmd-hook]]))
+
+(defn or-cmd
+  "or <text> # if piped args passed in "
+  [{:keys [raw match]}]
+  (info "or:" raw match)
+  (if (or (nil? raw) (empty? raw))
+    match
+    raw))
+
+(cmd-hook ["or" #"^or$"]
+  _ or-cmd)
