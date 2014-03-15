9721122c2a8a82e9d7f7db27ec69f2dfb365b054
Trevor Hartman <trevorhartman@gmail.com>
2014-03-15 01:09:48 -0600

    Move history observer into yetibot.core

--- src/yetibot/observers/history.clj
+++
@@ -1,9 +0,0 @@
-(ns yetibot.observers.history
-  (:require
-    [yetibot.core.models.history :as h]
-    [yetibot.core.hooks :refer [obs-hook]]))
-
-(obs-hook #{:message}
-          (fn [event-info]
-            (h/add {:user-id (-> event-info :user :id str)
-                    :body (:body event-info)})))
