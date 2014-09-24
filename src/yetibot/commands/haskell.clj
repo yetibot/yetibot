9ce3171267561602c43bca8238b7ffe30a695a89
Trevor Hartman <trevorhartman@gmail.com>
2014-09-24 15:07:45 -0600

    Update haskell command for new endpoint

--- src/yetibot/commands/haskell.clj
+++ src/yetibot/commands/haskell.clj
@@ -1,15 +1,26 @@
 (ns yetibot.commands.haskell
   (:require
-    [yetibot.core.hooks :refer [cmd-hook]]
-    [yetibot.core.util.http :refer [get-json encode]]))
+    [clj-http.client :as client]
+    [yetibot.core.hooks :refer [cmd-hook]]))
 
-(def endpoint "http://tryhaskell.org/haskell.json?method=eval&random=0.6923271066043526&expr=")
+(def endpoint "http://tryhaskell.org/eval")
+
+(defn hs-eval [exp]
+  (client/post
+    endpoint
+    {:throw-entire-message? true
+     :accept "application/json, text/javascript, */*; q=0.01"
+     :content-type "application/x-www-form-urlencoded"
+     :as :json
+     :form-params {:exp exp}}))
 
 (defn haskell-cmd
   "hs <expression> # evaluate haskell expression"
   [{:keys [match]}]
-  (let [json (get-json (str endpoint (encode match)))]
-    ((juxt :type :result) json)))
+  (let [json (hs-eval match)]
+    (if-let [success (-> json :body :success)]
+      ((juxt :type :value) success)
+      "Error: is tryhaskell.org down?")))
 
 (cmd-hook #"hs"
           #".*" haskell-cmd)
