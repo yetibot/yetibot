2844f4b60d50e12dfc37d9b7a293ab7c387c8b90
Trevor Hartman <trevorhartman@gmail.com>
2014-10-01 20:58:21 -0600

    Use new history map in that; use yetibot.core 0.2.2

--- src/yetibot/commands/that.clj
+++ src/yetibot/commands/that.clj
@@ -8,14 +8,14 @@
   "that cmd # retrieve last command from history"
   [{:keys [chat-source]}]
   (if-let [cmds (h/cmd-only-items chat-source)]
-    (-> cmds second second) ; the first result will be this cmd itself so use the second
+    (-> cmds second :history/body) ; the first result will be this cmd itself so use the second
     "Couldn't find a command in the last 100 history items :("))
 
 (defn that-cmd
   "that # retrieve last non-command chat from history"
   [{:keys [chat-source]}]
   (if-let [nocmd (h/non-cmd-items chat-source)]
-    (-> nocmd first second)
+    (-> nocmd first :history/body)
     "Couldn't find a non-command in the last 100 history items :("))
 
 (cmd-hook ["that" #"^that$"]
