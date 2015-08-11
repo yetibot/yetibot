87bfc626b036506e500e26f8ceb8cd20ed3a6055
Trevor Hartman <trevorhartman@gmail.com>
2015-08-11 09:57:31 -0600

    Update chatoms api endpoint

--- src/yetibot/commands/chatoms.clj
+++ src/yetibot/commands/chatoms.clj
@@ -3,7 +3,7 @@
     [yetibot.core.hooks :refer [cmd-hook]]
     [yetibot.core.util.http :refer [get-json]]))
 
-(def endpoint "http://chatoms.com/chatom.json?Normal=5&Fun=20&Philosophy=10&Out+There=4&Love=2&Personal=2")
+(def endpoint "http://www.chatoms.com/chatom.json?Normal=5&Fun=20&Philosophy=10&Out+There=4&Love=2&Personal=2")
 
 (defn chat-cmd
   "chat # ask a question from chatoms.com"
