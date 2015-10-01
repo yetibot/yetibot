5116639554b111bdeb54666008b13e612c47af72
Trevor Hartman <trevorhartman@gmail.com>
2015-10-01 14:57:28 -0600

    Improve `that` cmd efficiency

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  :welcome (println "Welcome to the yetibot development REPL!")}
   :jvm-opts ["-server" "-Xmx2G"]
   :dependencies [[org.clojure/clojure "1.6.0"],
-                 [yetibot.core "0.2.44"]
+                 [yetibot.core "0.2.45"]
 
                  ; apis
                  [tentacles "0.3.0"]
--- src/yetibot/commands/that.clj
+++ src/yetibot/commands/that.clj
@@ -7,16 +7,20 @@
 (defn that-with-cmd-cmd
   "that cmd # retrieve last command from history"
   [{:keys [chat-source]}]
-  (if-let [cmds (h/cmd-only-items chat-source)]
-    (-> cmds second :history/body) ; the first result will be this cmd itself so use the second
-    "Couldn't find a command in the last 100 history items :("))
+  ; the cmd used to call this will be in history, so get the last two then get
+  ; the first for it
+  (-> (h/last-chat-for-room chat-source true 2)
+      h/touch-all
+      first
+      :history/body))
 
 (defn that-cmd
   "that # retrieve last non-command chat from history"
   [{:keys [chat-source]}]
-  (if-let [nocmd (h/non-cmd-items chat-source)]
-    (-> nocmd first :history/body)
-    "Couldn't find a non-command in the last 100 history items :("))
+  (-> (h/last-chat-for-room chat-source false)
+            h/touch-all
+            first
+            :history/body))
 
 (cmd-hook ["that" #"^that$"]
   #"cmd" that-with-cmd-cmd
