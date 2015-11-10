3c478860b8ef2a2b29b1a1d3df1329f93d928425
Trevor Hartman <trevorhartman@gmail.com>
2015-11-09 19:01:11 -0500

    Move web to yetibot.core

--- project.clj
+++ project.clj
@@ -31,30 +31,14 @@
 
                  ; [incanter "1.4.0"]
 
-                 ; web
-                 [selmer "0.8.2"]
-                 [compojure "1.3.4"]
-                 [prone "0.8.2"]
-                 [hiccup "1.0.5"]
-                 ; [lib-noir "0.9.9" :exclusions [[org.clojure/tools.namespace]]]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [markdown-clj "0.9.66"]
-
-                 ; web/ring
-                 [ring/ring-json "0.3.1"]
-                 [ring/ring-core "1.4.0-RC1"]
-                 [ring/ring-jetty-adapter "1.4.0-RC1"]
-                 ; [info.sunng/ring-jetty9-adapter "0.8.4"]
-                 [ring/ring-defaults "0.1.5"]
-                 [ring/ring-session-timeout "0.1.0"]
-                 [metosin/ring-middleware-format "0.6.0"]
-                 [metosin/ring-http-response "0.6.2"]
-
                  ]
   :plugins [[lein-ring "0.9.5"]]
-  :pedantic :warn
+
+  :pedantic :ignore
+
   :ring {:handler yetibot.webapp.handler/app
          :init    yetibot.webapp.handler/init
          :destroy yetibot.webapp.handler/destroy
          :uberwar-name "yetibot.war"}
+
   :main yetibot.core.init)
