5bfedbc2aef1f694b4da3f166841535f5c03c285
Trevor Hartman <trevorhartman@gmail.com>
2016-01-07 09:39:13 -0700

    Tag snapshot versions with "snapshot" in Docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -15,10 +15,11 @@ version=`lein version`
 echo $version
 
 if [[ "$version" =~ "SNAPSHOT" ]]; then
-  echo "Snapshot"
+  echo "Snapshot version, tagging docker snapshot and pushing..."
+  docker tag devth/yetibot:$version devth/yetibot:snapshot
+  docker push devth/yetibot:snapshot
 else
   echo "Release version, tagging docker latest and pushing..."
-  docker images
   docker tag devth/yetibot:$version devth/yetibot:latest
   docker push devth/yetibot:latest
 fi
