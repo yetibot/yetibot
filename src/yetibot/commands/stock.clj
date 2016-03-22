2ad2e83ae815ab86ee499546dc044baa71c4a394
Paul Son <pson@ebay.com>
2016-03-22 13:37:23 -0700

    changed stock command to use google finance api

--- src/yetibot/commands/stock.clj
+++ src/yetibot/commands/stock.clj
@@ -11,27 +11,28 @@
   [stock-symbol]
   (str "http://www.google.com/finance/info?infotype=infoquoteall&q=" stock-symbol))
 
-(defn get-quote
-  "Gets response from Google Finance and retrieves json"
-  [stock-symbol]
-  (let [resp (client/get (endpoint stock-symbol))]
-    (-> resp
-        :body
-        (s/replace-first "//" "")
-        json/parse-string)))
+(defn get-body
+  "Gets json body from response"
+  [json]
+  (-> json
+      :body
+      (s/replace-first "//" "")
+      json/parse-string))
 
 (defn parse-quote
   "Parses most relevant information from json"
   [stock-symbol]
-  (let [quote (get-quote stock-symbol)]
-    (->> quote
-         first
-         keywordize-keys
-         ((juxt :name :l :hi :lo :mc :cp))
-         (interleave ["Name:" "Last Price:" "High:" "Low:" "Market Cap:" "Change Percent:"])
-         (partition 2)
-         (map #(s/join " " %)))))
-
+  (let [response (client/get (endpoint stock-symbol){:throw-exceptions false})]
+    (if (= (:status response) 200)
+      (let [quote (get-body response)]
+        (->> quote
+             first
+             keywordize-keys
+             ((juxt :name :l :hi :lo :mc :cp :ecp))
+             (interleave ["Name:" "Last Price:" "High:" "Low:" "Market Cap:" "Change Percent:" "After Hours Change Percent:"])
+             (partition 2)
+             (map #(s/join " " %))))
+      (str "Unable to find symbol for " stock-symbol ". Try another symbol such as MSFT or AAPL."))))
 
 (defn stock-cmd
   "stock <symbol> # displays current value in market"
