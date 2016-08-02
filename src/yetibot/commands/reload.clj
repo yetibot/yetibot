8939fffcdf7fb82cc0582404cbe0743ed39d6707
Trevor Hartman <trevorhartman@gmail.com>
2016-08-02 15:22:29 -0600

    Fix reload command

--- src/yetibot/commands/reload.clj
+++ src/yetibot/commands/reload.clj
@@ -1,23 +1,23 @@
 (ns yetibot.commands.reload
   (:require
     [clojure.string :as s]
+    [yetibot.core.loader :refer [find-and-load-namespaces]]
     [yetibot.core.hooks :refer [cmd-hook]]))
 
 (defn reload-all-cmd
   "reload # reload all of YetiBot's commands and observers"
   {:yb/cat #{:util}}
   [_]
-  (yetibot.core.loader/reload-all-yetibot)
-  "Reload complete.")
+  "No longer implemented")
 
 (defn reload-cmd
   "reload <namespace pattern> # reload a specific namespace"
   {:yb/cat #{:util}}
   [{:keys [match]}]
   (let [re (re-pattern (format ".*%s.*" match))
-        matched (yetibot.core.loader/find-and-load-namespaces [re])]
+        matched (find-and-load-namespaces [re])]
     (format "Reloaded namespaces:\n  %s" (s/join "\n  " matched))))
 
 (cmd-hook #"reload"
-          #".+" reload-cmd
-          _ reload-all-cmd)
+  #".+" reload-cmd
+  _ reload-all-cmd)
