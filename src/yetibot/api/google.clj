9d8f78191dcdaa4718d4a83383c251865d3cb260
luke <nerorevenge@gmail.com>
2016-10-05 01:09:54 -0400

    Final draft of the code  - inclusion of cli-options  - mutatble state now on the client side of things  - tests need to be rewritten/ moved

--- src/yetibot/api/google.clj
+++ src/yetibot/api/google.clj
@@ -1,32 +1,18 @@
 (ns yetibot.api.google
   (:require
-   [taoensso.timbre :refer [warn info]]
+   [taoensso.timbre :refer [info warn]]
    [clojure.string :as s]
    [clojure.data.json :as json]
    [clj-http.client :as client]
    [yetibot.core.config :refer [conf-valid? config-for-ns]]))
 
-
 (defonce api-url "https://www.googleapis.com/customsearch/v1?parameters")
 
-(defonce options (atom {}))
-
-(def accepted-keywords {})
-(declare set-option-value)
-
 (defonce messages
-  {:error-message "Values has to be one of the following: ",
-   :no-options-in-config-file  "No google options set in config file."
-   :option-not-found "Option not found"
-   :extra-set "Can also be set via %s as an extra arg to search command"})
+  {:error-message "Values has to be one of the following: "})
 
 (defonce display-order {:normal [:title :link :snippet]
                         :image  [:title :snippet :link]})
-
-;; this is a defn because accepted keywords
-;; is redenfined again towards the bottom
-(defn valid-keywords-list []
-  (keys accepted-keywords))
 
 (def config (config-for-ns))
 
@@ -37,54 +23,7 @@
        (= (count config-values) 2))))
 
 (defn populate-options-from-config []
-  "This loads the custom-engine options set
-  in the config file and load it into the options
-  atom"
-  (let [options (:options config)]
-    (if-not (seq options)
-      (info (:no-options-in-config-file messages))
-      (reduce-kv (fn [_ k v]
-                   (-> (s/trim k)
-                       (s/lower-case)
-                       (set-option-value v)
-                       (info))) [] options))))
-
-(defn- fetch-option-if-exists
-  [option]
-  (get accepted-keywords option))
-
-(defn- validate-option-value
-  [option value]
-  (if-let [option_ (fetch-option-if-exists option)]
-    (re-matches (:validation option_) value)
-    :option-does-not-exist))
-
-(defn set-option-value
-  "This is responsible for checking if
-  a key exists, if it does, it validates the
-  value given"
-  [option value]
-  (condp = (validate-option-value option value)
-    :option-does-not-exist nil
-    nil (get-in accepted-keywords [option :error-message])
-    (do (swap! options
-               assoc (get-in accepted-keywords [option :keyword]) value)
-        (str option " successfully set to " value))))
-
-(defn get-info
-  "Gets info about an option"
-  [option]
-  (:info-message (fetch-option-if-exists option)))
-
-(defn state-of-set-options []
-  "gives back contents of the options
-  atom. If empty gives back nil"
-  (if-not (seq @options)
-    nil
-    @options))
-
-(defn reset-options []
-  (reset! options {}))
+  (:options config))
 
 (defn format-result
   "format a single query result"
@@ -108,15 +47,16 @@
 
 (defn search
   "main search function,
-  extra refers to map of extra params to the search api
+  args refer to map of extra params to the search api
   order refers to the the way result is display (look
-  at display-order var)" [q & {:keys [extra order] :or {extra {} order :normal}}]
+  at display-order var)"
+  [q & {:keys [order args] :or {args {} order :normal}}]
   (info "Google search for: " q)
   (let [query-params  {:q q
                        :key (:api-key config)
                        :cx (:custom-search-engine-id config)}
         options {:query-params
-                 (merge query-params extra @options)}]
+                 (merge query-params args)}]
     (do
       (info options)
     (try
@@ -128,226 +68,200 @@
         (warn "Google: caught " e)
         nil)))))
 
-(defn image-search [q]
+(defn image-search [q & {:keys [args] :or {args {}}}]
   (let [param {"searchType" "image"}]
-    (search q :extra param :order :image)))
+    (search q :order :image
+              :args (merge args param))))
 
 (def accepted-keywords
-  {"c2coff"
+  {:c2coff
     {:validation #"^0|1$"
      :error-message (str (:error-message messages) "0,1")
      :info-message "Enables or Disables Simplified and Traditional Chinese Search"
-     :extra (format (:extra-set messages) "--c2c")
      :keyword "c2coff"},
 
-   "imgcolortype"
+   :imgcolortype
     {:validation #"^mono|gray|color$",
      :error-message (str (:error-message messages) "mono, gray, color")
      :info-message "Returns mono, gray or color images for image-search"
-     :extra (format (:extra-set messages) "-c, --color")
      :keyword "imgColorType"},
 
-   "cr"
+   :cr
     {:validation #"^country[A-Z]{2}(:?(:?\|\||&&)country[A-Z]{2})*$"
      :error-message
      "Examples of valid input are `countryAB' or `countryAB&&countryZN'"
      :info-message
      "This parameter restricts search to documents originating from a certain country"
-     :extra (format (:extra-set messages) "-C, --cr")
      :keyword "cr"},
 
-   "filter"
+   :filter
      {:validation #"^0|1$"
       :error-message (str (:error-message messages) "0,1")
       :info-message "Turns off or on duplicate content filter"
-      :extra (format (:extra-set messages) "-f, --filter")
       :keyword "filter"},
 
-   "g1"
+   :g1
     {:validation #"^[a-z]{2}$"
      :error-message "Example of correct input: uk"
      :info-message
      (str "Specifies the geolocation of end-user \n"
           "which would ideally result in location specific results")
-     :extra (format (:extra-set messages) "-g, --g1")
      :keyword "g1"},
 
-   "h1"
+   :h1
     {:validation #"^[a-z]{2}|zh-Han[s|t]$"
      :error-message "Valid example is 'fr' for french"
      :info-message
      "Specifies the language for the search interface and improves search results."
-     :extra (format (:extra-set messages) "--h1")
      :keyword "h1"
     },
 
-   "hq"
+   :hq
     {:validation #".+"
      :error-message "Valid input is a non empty string"
      :info-message "Appends option value to query similar to an AND operator \n"
-     :extra (format (:extra-set messages) "-A,--and")
      :keyword "hq"
      },
 
-   "daterestrict"
+   :daterestrict
     {:validation #"^(?:d|w|m|y)\d+$"
      :error-message
      (str "Valid values are of format (d|w|m|y)<number>\n"
           "d,w,m,y refers to days, weeks, months and years")
      :info-message
      "Restricts results to URLS based on date"
-     :extra (format (:extra-set messages) "-d, --date")
      :keyword "dateRestrict"
     },
 
-   "excludeterms"
+   :excludeterms
     {:validation #".+"
      :error-message "Valid input is a non empty string"
      :info-message "Identifies a word/phrase that should'nt appear in results\n"
-     :extra (format (:extra-set messages) "-x, --exclude")
      :keyword "excludeTerms"
     },
 
-   "exactterms"
+   :exactterms
     {:validation #".+"
      :error-message "Valid input is a non empty string"
      :info-message "Identifies a word/phrase that should appear in results\n"
-     :extra (format (:extra-set messages) "-e, --exact")
      :keyword "exactTerms"
     },
 
-   "highrange"
+   :highrange
     {:validation #"^\d+$"
      :error-message "Valid input is a number."
      :info-message  "Specifies the ending valude for a search range"
-     :extra (format (:extra-set messages) "--hr")
      :keyword "highRange"
      },
 
-   "lowrange"
+   :lowrange
     {:validation #"^\d+$"
      :error-message "Valid input is a number."
      :info-message "Specifies the ending valude for a search range"
-     :extra (format (:extra-set messages) "--lr")
      :keyword "lowRange"
     },
 
-   "googlehost"
+   :googlehost
     {:validation #"^google\.(?:com|[a-z]{2})$"
      :error-message "Valid values include 'google.de', 'google.fr'"
      :info-message "Specifies local google domain to perform the search"
-     :extra (format (:extra-set messages) "--ghost")
      :keyword "keyword"
      },
 
-   "filetype"
+   :filetype
     {:validation #"^\.\S+$"
      :error-message "Valid values include '.pdf', '.html', '.clj' etc"
      :info-message "Restricts results to files of a specific extension"
-     :extra (format (:extra-set messages) "-F, --ft")
      :keyword "fileType"
      },
 
-   "imgdominantcolor"
+   :imgdominantcolor
     {:validation #"^black|blue|brown|gray|green|pink|purple|teal|white|yellow$",
      :error-message (str (:error-message messages)
                          "black, blue, brown, gray, green, pink, purple, teal,"
                          "white, yellow")
      :info-message "Returns images of a specific dominant color"
-     :extra (format (:extra-set messages) "--dominantcolor")
      :keyword "imgDominantColor"},
 
-   "imgtype"
+   :imgtype
     {:validation ,#"^clipart|face|lineart|news|photo$"
      :error-message (str (:error-message messages)
                        "clipart, face, lineart, news, photo")
      :info-message "Returns images of a specific type"
-     :extra (format (:extra-set messages) "-t, --type")
      :keyword "imgType"},
 
-   "imgsize"
+   :imgsize
     {:validation ,#"^huge|icon|large|medium|small|xlarge|xxlarge$"
      :error-message (str (:error-message messages)
                          "huge, icon, large, medium, small,"
                          "xlarge, xxlarge")
      :info-message "Returns images of a specific size"
-     :extra (format (:extra-set messages) "-z, --size")
      :keyword "imgSize"},
 
-   "linksite"
+   :linksite
     {:validation ,#".*"
      :error-message "Valid input is a website link"
      :info-message
      "Specifies all search results should contain a link to a specific site"
-     :extra (format (:extra-set messages) "-l, --link")
      :keyword "linkSite"},
 
-   "safe"
+   :safe
     {:validation ,#"^high|medium|off$"
      :error-message (str (:error-message messages) "high, medium, off")
      :info-message "Search Safety Level"
-     :extra (format (:extra-set messages) "-o, --safe")
      :keyword "safe"},
 
-   "relatedsite"
+   :relatedsite
     {:validation #".*"
      :error-message "Valid input is a website link"
      :info-message
      "Specifies all search results should be pages related to a url"
-     :extra (format (:extra-set messages) "-r, --relsite")
      :keyword "relatedSite"},
 
-   "sitesearch"
+   :sitesearch
     {:validation #".*"
      :error-message "Valid input is a website link"
      :info-message
      "Specifies all search results should be pages from a given site"
-     :extra (format (:extra-set messages) "-s, --site")
      :keyword "siteSearch"},
 
-   "sitesearchfilter"
+   :sitesearchfilter
     {:validation #"^e|i$"
      :error-message  (str (:error-message messages) "e, i")
      :info-message
      "includes/excludes results from site in siteSearch param"
-     :extra (format (:extra-set messages) "-t, --sitefilter")
      :keyword "siteSearchFilter"},
 
-   "rights"
+   :rights
     {:validation #".*"
      :error-message (str "Supported values include: "
                          "cc_publicdomain, cc_attribute, cc_sharealike,"
                          "cc_noncommercial, cc_nonderived"
                          ", and combinations of these.")
      :info-message "Filter based on licensing"
-     :extra (format (:extra-set messages) "-i, --license")
      :keyword "rights"},
 
-   "sort"
+   :sort
     {:validation #".*"
      :error-message "Valid input is a nonempty string"
      :info-message "The sort expression to apply to filters"
-     :extra (format (:extra-set messages) "-q, --sort")
      :keyword "sort"},
 
-   "num"
+   :num
     {:validation #"^\d|10$"
      :error-message "Valid input is a number between 1 to 10"
      :info-message "Number of search results to return"
-     :extra (format (:extra-set messages) "-n, --num")
      :keyword "num"},
 
-   "start"
+   :start
     {:validation #"^\d+$"
      :error-message "Valid input is a number"
      :info-message "The index of the first result to return"
-     :extra (format (:extra-set messages) "-y, --start")
      :keyword "start"},
 
-   "orterms"
+   :orterms
     {:validation #".+"
      :error-message "Valid input is a non empty string"
      :info-message "Appends option value to query similar to an OR operator "
-     :extra (format (:extra-set messages) "-O,--or")
      :keyword "orTerms"
      }})
--- src/yetibot/commands/google.clj
+++ src/yetibot/commands/google.clj
@@ -1,11 +1,42 @@
 (ns yetibot.commands.google
   (:require
     [taoensso.timbre :refer [info]]
-    [clojure.string :refer [join trim lower-case]]
+    [clojure.string :refer [join trim lower-case split]]
+    [clojure.tools.cli :refer [parse-opts]]
     [yetibot.api.google :as api]
     [yetibot.core.hooks :refer [cmd-hook suppress]]))
 
-(api/populate-options-from-config)
+(defonce options (atom {}))
+
+(defonce cli-args
+  {:c2coff       [nil "--c2c"],     :imgcolortype ["-c" "--color"],
+   :cr           ["-C" "--cr"],     :filter       ["-f" "--filter"],
+   :g1           ["-g" "--g1"],     :h1           [nil "--h1"],
+   :hq           ["-A" "--and"],    :daterestrict ["-d" "--date"],
+   :excludeterms ["-x" "--exclude"],:exactterms   ["-e" "--exact"],
+   :highrange    [nil "--hr"],      :lowrange     [nil "--lr"],
+   :googlehost   [nil "--ghost"],   :filetype     ["-F" " --ft"],
+   :imgtype      ["-t" "--type"],   :imgsize      ["-z"  "--size"],
+   :linksite     ["-l"  "--link"],  :safe         ["-o" "--safe"],
+   :relatedsite  ["-r" "--relsite"],:sitesearch   ["-s" "--site"],
+   :rights       ["-i" "--license"],:sort         ["-q" "--sort"],
+   :num          ["-n" " --num"],   :start        ["-y" "--start"],
+   :imgdominantcolor [nil "--dominantcolor"],
+   :sitesearchfilter ["-t" "--sitefilter"],
+   :orterms          ["-O" "--or"],
+  })
+
+(defonce valid-keywords-list
+  (keys cli-args))
+
+(defonce built-cli-options
+  (reduce-kv
+   (fn [m k v]
+     (conj m (conj (k cli-args) (:info-message v)
+                   :id k
+                   :validate [#(re-matches (:validation v) %)
+                              (:error-message v)])))
+   [] api/accepted-keywords))
 
 (def messages
   {:search "Google returned no results!!"
@@ -15,61 +46,134 @@
    :option-not-found "Option not found"
    :no-options-set "No options have been set."
    :reset "Options has been reset"
+   :empty-query "Query cannot be left empty"
+   :invalid-args "Check options given"
+   :com-not-found  (str "Command does not exist."
+                        " Type in !help google for valid commands")
+   :no-options-in-config-file  "No google options set in config file."
    })
 
-(defn no-command-found
-  [_]
-  (str "Command does not exist."
-       " Type in !help google for valid commands"))
+(defn command-not-found [_]
+  (:com-not-found messages))
 
-(defn clean [str]
-  (trim (lower-case str)))
+(defn- state-of-set-options []
+  "gives back contents of the options
+  atom. If empty gives back nil"
+  (if-not (seq @options)
+    nil
+    @options))
+
+(defn- fetch-option-if-exists
+  [option]
+  (get api/accepted-keywords option))
+
+(defn- validate-option-value
+  [option value]
+  (if-let [option_ (fetch-option-if-exists option)]
+    (re-matches (:validation option_) value)
+    :option-does-not-exist))
+
+(defn- set-option-value
+  "This is responsible for checking if
+  a key exists, if it does, it validates the
+  value given"
+  [option value]
+  (condp = (validate-option-value option value)
+    :option-does-not-exist nil
+    nil (get-in api/accepted-keywords [option :error-message])
+    (do (swap! options
+               assoc option value)
+        (str option " successfully set to " value))))
+
+(defn get-info
+  "Gets info about an option"
+  [option]
+  (if-let [value (fetch-option-if-exists option)]
+    (:info-message value)))
+
+(defn convert-keys-into-google-keywords
+  [obj]
+  (reduce-kv (fn [m k v]
+               (assoc m
+                      (get-in api/accepted-keywords [k :keyword]) v)) {}))
+
+(defn options-processor [string]
+  (let [output (parse-opts string built-cli-options)]
+    (if-let [error-messages (:errors output)]
+      {:errors error-messages}
+      {:query (join " " (:arguments output))
+       :args (convert-keys-into-google-keywords (:options output))})))
+
+(defn common-search-function
+  [query & {:keys [order sfunction]
+   :or {order :normal sfunction api/search}}]
+  (let [proc-query (options-processor query)]
+    (cond
+      (empty? (:query proc-query)) (:empty-query messages)
+      (:errors proc-query) (join "\n" (:errors proc-query))
+      :else
+      (let [args (:args proc-query)
+            results (sfunction (:query proc-query)
+                               :args (merge @options args))
+            count (get-in results [:searchInformation
+                                   :totalResults])]
+        (condp = count
+          nil (:google-died messages)
+          "0" (:search messages)
+          (api/format-results (:items results) :order order))))))
 
 (defn search
   "google search <query> # plain google search"
   [{[_ query] :match}]
-  (let [results (api/search query)
-        count (get-in results [:searchInformation
-                               :totalResults])]
-    (condp = count
-      nil (:google-died messages)
-      "0" (:search messages)
-      (api/format-results (:items results)))))
+  ()
+  (common-search-function query))
 
 (defn image-search
   "google image <query> # image search"
   [{[_ query] :match}]
-  (let [results (api/image-search query)
-        count (get-in results [:searchInformation
-                               :totalResults])]
-    (condp = count
-      nil (:google-died messages)
-      "0" (:image messages)
-      (api/format-results (:items results) :order :image))))
+  (common-search-function query
+                          :sfunction api/image-search
+                          :order :image))
 
+(defn- load-options-from-file-into-atom
+  []
+  (let [options  (api/populate-options-from-config)]
+    (if (empty? options)
+      (info (:no-options-in-config-file messages))
+      (doall (map (fn [[k v]]
+                    (if-let [result (set-option-value k v)]
+                      (info result)
+                      (info k ":" (:option-not-found messages))))
+                  options)))))
+
+(load-options-from-file-into-atom)
+(defonce copy-of-valid-file-options
+  @options)
+
+;; stay
 (defn option-info
   "google option-info <query> #display info for option"
   [{[_ query] :match}]
-  (if-let [info (api/get-info (clean query))]
+  (if-let [info (get-info (keyword query))]
     info
     (:option-not-found messages)))
 
 (defn show-custom-keywords-list
   "google show options # show list of valid options"
   [_]
-  (join ", " (api/valid-keywords-list)))
+  (join ", " (map name valid-keywords-list)))
 
 (defn set-option
   "google set <option> <value>"
   [{[_ option value] :match}]
-  (if-let [message (api/set-option-value (clean option) value)]
+  (if-let [message (set-option-value (keyword option) value)]
     message
     (:option-not-found messages)))
 
 (defn show-set-options
   "google show set options"
   [_]
-  (if-let [options-set (api/state-of-set-options)]
+  (if-let [options-set (state-of-set-options)]
     (->> options-set
          (map #(str " - " (first %) " has been set to " (second %)))
          (join "\n"))
@@ -79,8 +183,13 @@
   "google reset options # current set options are vanquished"
   [_]
   (do
-    (api/reset-options)
+    (reset! options {})
     (:reset messages)))
+
+(defn restore-options
+  "google restore options # restore from file config"
+  [_]
+  (reset! options copy-of-valid-file-options))
 
 (if (api/configured?)
   (cmd-hook #"google"
@@ -91,5 +200,6 @@
             #"^set\s+(\S+)\s+(\S+)" set-option
             #"^show\s+set\s+options$" show-set-options
             #"^reset\s+options$" reset-options
-            #".+" no-command-found)
+            #"^restore\s+options$" restore-options
+            #".+" command-not-found)
   (info "Google is not configured."))
