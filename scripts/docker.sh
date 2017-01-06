31d28a690ac01a70acb6be1e1d6d81496959cd64
Trevor Hartman <trevorhartman@gmail.com>
2017-01-05 17:11:25 -0700

    Fixup shellcheck linting issues
    Ref #607

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -13,16 +13,16 @@ set -ev
   lein docker push
 
   # check to see if it's a release version
-  version=`lein version`
-  echo $version
+  version=$(lein version)
+  echo "$version"
 
-  if [[ "$version" =~ "SNAPSHOT" ]]; then
+  if [[ "$version" =~ SNAPSHOT ]]; then
     echo "Snapshot version, tagging docker snapshot and pushing..."
-    docker tag devth/yetibot:$version devth/yetibot:snapshot
+    docker tag devth/yetibot:"$version" devth/yetibot:snapshot
     docker push devth/yetibot:snapshot
   else
     echo "Release version, tagging docker latest and pushing..."
-    docker tag devth/yetibot:$version devth/yetibot:latest
+    docker tag devth/yetibot:"$version" devth/yetibot:latest
     docker push devth/yetibot:latest
   fi
 
