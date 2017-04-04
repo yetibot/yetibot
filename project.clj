6c482bb889580679a3e5bde106b04ff96de2cddf
jkieberk <jkieberking@gmail.com>
2017-04-03 23:47:33 -0400

    Fixed Poorly Formatted ']'

--- project.clj
+++ project.clj
@@ -30,8 +30,7 @@
                  [robert/bruce "0.8.0"]
                  
                  ; emojis
-                 [com.vdurmont/emoji-java "3.2.0"]
-                 ]
+                 [com.vdurmont/emoji-java "3.2.0"]]
   :plugins [[lein-exec "0.3.5"]
             [lein-environ "1.0.3"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
