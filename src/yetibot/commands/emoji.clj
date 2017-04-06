0d04e1d8239869a16cd4b0b399cbfa455ca362f1
jkieberk <jkieberking@gmail.com>
2017-04-06 07:44:01 -0400

    Add emoji command (#651)
    * Initial Emoji Databse Commit
    
    - current commands enabled: tag-search
    
    * Finished Emoji Database
    
    Command is: `!emoji`
    
    `!emoji` Commands are:
    
    `!emoji tag [-i] <query>` - search by tag
    `!emoji description [i] <query>` - search by description
    `!emoji alias <query> - search by alias
    
    Note: The `-i` flag returns both the unicode and a list of aliases.
          Ommiting the flag returns just the unicode.
    
    * Trailing space
    
    * Requested Fixes
    
    -remove unused json require
    -cache emojis forever
    -replace all instances of `meme` with `emoji` (woops)
    -require com.vdurmont.emoji in project.clj
    
    * Requested Fixes #2
    
    - Removed trailing whitespace/lines > 80 char
    - Added `!emoji tags` command to list all tags
    - Renamed search-by-description to just search
    - Added `!emoji list` command to list all emojis
      (with -i flag to liast aliases as well)
    
    * Fixed Poorly Formatted ']'
    
    * Added rand Command and Moved Around Command Hook
    
    - Added `!emoji rand` command to get a random emoji
            (with -i flag to include aliases)
    - Moved around command hook commands to follow function locations
      in the file
    
    * Removed `rand` Emoji Command
    
    * Added Missing Parameter

---
+++ src/yetibot/commands/emoji.clj
@@ -0,0 +1,117 @@
+(ns yetibot.commands.emoji
+  (:require
+    [taoensso.timbre :refer [info warn error]]
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [clojure.core.memoize :as m])
+  (:import
+   [com.vdurmont.emoji EmojiManager]))
+
+(defn get-all-emojis
+  "get all emoji java objects"
+  []
+  (EmojiManager/getAll))
+
+(defn parse-all-emojis
+  "create sequence of emojis"
+  []
+  (map (fn [emoji] {:unicode (.getUnicode emoji)
+                    :description (.getDescription emoji)
+                    :aliases (.getAliases emoji)
+                    :tags (.getTags emoji)
+                    }) (get-all-emojis)))
+
+(defonce emojis (parse-all-emojis))
+
+(defn all-tags
+  "emoji tags # return all known tags"
+  {:yb/cat #{:fun :img :emoji}}
+  [_]
+  (-> (reduce (fn [return-set emoji]
+                (let [tag-vector (:tags emoji)
+                      tag-hash-set (set tag-vector)]
+                  (clojure.set/union return-set tag-hash-set)))
+              #{}
+              emojis)
+      sort))
+
+(defn list-emojis
+  "emoji list [-i] # list all known emojis
+
+   [-i (info)] is optional, if set will return the unicode
+   as well as a vector of its aliases"
+  {:yb/cat #{:fun :img :emoji}}
+  [{[_ info-flag] :match}]
+  (let [emoji-keys (filter identity [:unicode (if info-flag :aliases)])
+        select-values (comp vals select-keys)
+        map-fn (fn [emoji]
+                    (clojure.string/join " " (select-values emoji emoji-keys)))]
+    (map map-fn emojis)))
+
+(defn filter-by-tag
+  "filter emojis by tag"
+  [tag]
+  (filter (fn [emoji] (some #{tag} (:tags emoji))) emojis))
+
+(defn filter-by-description
+  "filter emojis by description"
+  [description]
+  (filter (fn [emoji]
+            (re-find (re-pattern description) (:description emoji)))
+          emojis))
+
+(defn filter-by-alias
+  "filter emojis by alias"
+  [alias]
+  (filter (fn [emoji]
+            (->
+             (filter (fn [aliases]
+                       (re-find (re-pattern (str "^" alias "$")) aliases))
+                     (:aliases emoji))
+             empty?
+             not))
+          emojis))
+
+(defn search-by-tag
+  "emoji tag <query> # query emojis by their tag
+
+   [-i (info)] is optional, if set will return the unicode
+   as well as a vector of its aliases"
+  {:yb/cat #{:fun :img :emoji}}
+  ([{[_ info-flag tag] :match}]
+   (if-let [found-emojis (filter-by-tag tag)]
+     (let [emoji-keys (filter identity [:unicode (if info-flag :aliases)])
+           select-values (comp vals select-keys) 
+           map-fn (fn [emoji]
+                    (clojure.string/join " " (select-values emoji emoji-keys)))]
+       (map map-fn found-emojis))
+     (str "Couldn't find any emojis with: \"" tag "\" tag"))))
+
+(defn search
+  "emoji search [-i] <query> # search emojis by their description
+
+   [-i (info)] is optional, if set will return the unicode
+   as well as a vector of its aliases"
+  {:yb/cat #{:fun :img :emoji}}
+  ([{[_ info-flag description] :match}]
+   (if-let [found-emojis (filter-by-description description)]
+     (let [emoji-keys (filter identity [:unicode (if info-flag :aliases)])
+           select-values (comp vals select-keys)
+           map-fn (fn [emoji]
+                    (clojure.string/join " " (select-values emoji emoji-keys)))]
+       (map map-fn found-emojis))
+     (str "Couldn't find any emojis with: \"" description "\" description"))))
+
+(defn search-by-alias
+  "emoji alias <query> # query an emoji by its alias (meant to return 1 emoji)"
+  {:yb/cat #{:fun :img :emoji}}
+  ([{[_ alias] :match}]
+   (if-let [found-emoji (first (filter-by-alias alias))]
+     [(:unicode found-emoji)]
+     (str "Couldn't find any emojis with: \"" alias "\" alias"))))
+
+(cmd-hook ["emoji" #"^emoji$"]
+          #"^tags$" all-tags
+          #"^list(\s-i)?$" list-emojis
+          #"^tag\s(-i\s)?(.+)$" search-by-tag
+          #"^search\s(-i\s)?(.+)$" search
+          #"^alias\s(.+)$" search-by-alias)
