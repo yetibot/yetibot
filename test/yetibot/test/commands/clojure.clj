2c899f6dfe0a4e0a282cd28f0ca849a3a11c9e66
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-08 14:26:45 -0300

    Rewrite clojure command test with midje

--- test/yetibot/test/commands/clojure.clj
+++ test/yetibot/test/commands/clojure.clj
@@ -1,8 +1,18 @@
 (ns yetibot.test.commands.clojure
   (:require
-    [clojure.test :refer :all]
+    [midje.sweet :refer [fact => anything]]
     [yetibot.commands.clojure :refer :all]
-    [yetibot.core.parser :refer [parse-and-eval]]))
+    [yetibot.core.util.http :refer [get-json map-to-query-string]]))
 
-(deftest test-clj
-  (is (= (parse-and-eval "clj (+ 1 2)") "3")))
+(fact "should eval as clojure code and return response from real service"
+  (clojure-cmd {:args "(* 10 2)"}) => "20")
+
+(fact "should eval as clojure code and return response"
+  (clojure-cmd {:args "(+ 1 2)"}) => "3"
+  (provided
+    (get-json anything) => {:expr "(+ 1 2)" :result "3"}))
+
+(fact "should return error message when request fail"
+  (clojure-cmd {:args "(+ 1 B)"}) => "Error"
+  (provided
+    (get-json anything) => {:error true :message "Error"}))
