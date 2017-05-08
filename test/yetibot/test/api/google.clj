85eb60f7a0996a46e66274ddf9c8d1c9152f61d6
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-08 11:23:36 -0300

    Rewrite google test with midje

--- test/yetibot/test/api/google.clj
+++ test/yetibot/test/api/google.clj
@@ -1,10 +1,9 @@
 (ns yetibot.test.api.google
   (:require
-   [clojure.test :refer :all]
+   [midje.sweet :refer [fact => anything =throws=>]]
    [clj-http.client :as client]
    [clojure.data.json :as json]
-   [yetibot.api.google :as api]
-   ))
+   [yetibot.api.google :as api]))
 
 (defonce test-call-response
    {:items [{:title "x-files"
@@ -20,39 +19,34 @@
 (defonce test-call-response-json
   {:body (json/write-str test-call-response)})
 
-(deftest format-result-normal-order-test
+(fact format-result-normal-order-test
   (let [test-data (nth test-items 0)]
-    (is (= (api/format-result test-data)
-           (str (:title test-data)
-                "\n" (:link test-data)
-                "\n" (:snippet test-data))))))
+    (api/format-result test-data) => (str (:title test-data)
+                                          "\n" (:link test-data)
+                                          "\n" (:snippet test-data))))
 
-(deftest format-result-image-order-test
+(fact format-result-image-order-test
   (let [test-data (nth test-items 0)]
-    (is (= (api/format-result test-data :order :image)
-           (str (:title test-data)
-                "\n" (:snippet test-data)
-                "\n" (:link test-data))))))
+    (api/format-result test-data :order :image) => (str (:title test-data)
+                                                        "\n" (:snippet test-data)
+                                                        "\n" (:link test-data))))
 
-(deftest format-results-normal-order-test
-  (is (= (api/format-results test-items)
-         [(str "1. " (api/format-result (nth test-items 0)) "\n\n")
-          (str "2. " (api/format-result (nth test-items 1)) "\n\n")])))
+(fact format-results-normal-order-test
+  (api/format-results test-items) => [(str "1. " (api/format-result (nth test-items 0)) "\n\n")
+                                      (str "2. " (api/format-result (nth test-items 1)) "\n\n")])
 
-(deftest format-results-image-order-test
-  (is (= (api/format-results test-items :order :image)
-         [(str "1. " (api/format-result (nth test-items 0)
-                                        :order :image) "\n\n")
-          (str "2. " (api/format-result (nth test-items 1)
-                                        :order :image) "\n\n")])))
+(fact format-results-image-order-test
+  (api/format-results test-items :order :image) => [(str "1. " (api/format-result (nth test-items 0)
+                                                                                  :order :image) "\n\n")
+                                                    (str "2. " (api/format-result (nth test-items 1)
+                                                                                  :order :image) "\n\n")])
 
-(deftest search-test
-  (with-redefs-fn
-    {#'client/get (fn [_ & _] test-call-response-json)}
-    #(is (= (api/search "query")
-            test-call-response))))
+(fact search-test
+  (api/search "query") => test-call-response
+  (provided
+    (client/get anything anything) => test-call-response-json))
 
-(deftest search-test-with-bad-http-return
-  (with-redefs-fn
-    {#'client/get (fn [_ & _] (throw (Exception. "Bad return")))}
-    #(is (nil? (api/search "query")))))
+(fact search-test-with-bad-http-return
+  (api/search "query") => nil?
+  (provided
+    (client/get anything anything) =throws=> (Exception. "Bad return")))
