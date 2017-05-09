36f50efdc26a49447c75634556d1222f1d5e03f4
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-09 16:31:13 -0300

    Add tests for json command

--- test/yetibot/test/commands/json.clj
+++ test/yetibot/test/commands/json.clj
@@ -1,8 +1,17 @@
 (ns yetibot.test.commands.json
   (:require
+    [clojure.data.json :as json]
     [midje.sweet :refer [fact =>]]
     [yetibot.commands.json :refer :all]))
 
-(fact test-should-convert-keys-to-keywords-when-parsing-json
+(fact convert-keys-to-keywords-when-parsing-json
   (let [args {:match ["" "{\"key\": \"value\"}"]}]
     (json-parse-cmd args) => {:key "value"}))
+
+(fact return-value-of-json-path
+  (let [args {:match ["" "$.key"] :opts (json/read-str "{\"key\": \"value\"}" :key-fn keyword)}]
+    (json-path-cmd args) => "value"))
+
+(fact return-error-when-json-is-invalid
+  (let [args {:match ["" "$.key"] :opts "{}"}]
+    (json-path-cmd args) => "Not a valid json data structure:\"{}\""))
