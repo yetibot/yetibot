774646573db8e8615105b2ce8dd592fed887c2d4
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-08 22:29:42 -0300

    Rewrite github tests with midje

--- test/yetibot/test/commands/github.clj
+++ test/yetibot/test/commands/github.clj
@@ -1,31 +1,22 @@
 (ns yetibot.test.commands.github
   (:require
-   [clojure.test :refer :all]
+   [midje.sweet :refer [fact => anything]]
    [yetibot.commands.github :refer [stats-cmd]]
    [yetibot.api.github :as gh]))
 
-(def dummy-poll-counter (atom 0))
 (def mock-poll-result {:a 1 :d 1 :c 1 :con 4})
 (def mock-repo-info ["wilfred" "tv"])
 
-(defn mock-stats-fn [_ _]
-  (if (= @dummy-poll-counter 2)
-    mock-poll-result
-    (swap! dummy-poll-counter inc)))
+(fact test-stats-cmd-polling-no-response
+  (stats-cmd {:match (cons "dummy" mock-repo-info)}) =>
+      (apply format "Crunching the latest data for `%s/%s`, try again in a few moments 🐌"
+             mock-repo-info)
+  (provided
+    (gh/sum-stats anything anything) => nil))
 
-(deftest test-stats-cmd-polling-no-response
-  (with-redefs-fn
-    {#'gh/sum-stats (fn [_ _] nil)}
-    #(is (= (stats-cmd {:match (cons "dummy" mock-repo-info)})
-            (apply format
-                   "Crunching the latest data for `%s/%s`, try again in a few moments 🐌"
-                   mock-repo-info)))))
-
-(deftest test-stats-cmd-polling-success-response
-  (with-redefs-fn
-    {#'gh/sum-stats mock-stats-fn}
-    #(is (= (stats-cmd {:match (cons "dummy" mock-repo-info)})
-            (apply format
-                   "%s/%s: %s commits, %s additions, %s deletions, %s contributors"
-             (into mock-repo-info (vals mock-poll-result)))))))
-
+(fact test-stats-cmd-polling-success-response
+  (stats-cmd {:match (cons "dummy" mock-repo-info)}) =>
+          (apply format "%s/%s: %s commits, %s additions, %s deletions, %s contributors"
+           (into mock-repo-info (vals mock-poll-result)))
+  (provided
+    (gh/sum-stats anything anything) => mock-poll-result))
