060c2b1c73936ae0b6cf6240d47669b13a424f25
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-08 22:48:04 -0300

    Rewrite scala tests with midje

--- test/yetibot/test/commands/scala.clj
+++ test/yetibot/test/commands/scala.clj
@@ -1,24 +1,22 @@
 (ns yetibot.test.commands.scala
   (:require
-    [clojure.test :refer :all]
+    [midje.sweet :refer [fact => truthy]]
     [yetibot.core.parser :refer [parser parse-and-eval]]
     [yetibot.commands.scala :refer :all]))
 
 (def eval-and-extract (comp extract-result try-scala))
 
-(deftest test-scala
-  (is (= '("2: Int" "4: Int")
-         (eval-and-extract "1 + 1; 2 + 2"))
-      "Valid expressions")
-  (is (= '("not found: value a" "not found: value b")
-         (eval-and-extract "a; b"))
-      "Compiler errors")
-  (is (= '("java.lang.ArithmeticException: / by zero")
-         (eval-and-extract "1 / 0"))
-      "Runtime errors")
-  (is (= '("\"version 2.11.8\": String")
-         (eval-and-extract "scala.util.Properties.versionString"))
-      "scalakata.EString"))
+(fact should-successfully-extract-valid-expression-test-scala
+  (eval-and-extract "1 + 1; 2 + 2") => '("2: Int" "4: Int"))
 
-(deftest with-pipes
-  (parser "scala \"true || false\""))
+(fact should-return-not-found-for-unknown-variables
+  (eval-and-extract "a; b") => '("not found: value a" "not found: value b"))
+
+(fact should-return-error-for-invalid-operations
+  (eval-and-extract "1 / 0") => '("java.lang.ArithmeticException: / by zero"))
+
+(fact should-evaluate-properties
+  (eval-and-extract "scala.util.Properties.versionString") => '("\"version 2.11.8\": String"))
+
+(fact should-allow-evaluation-with-pipes
+  (parser "scala \"true || false\"") => truthy)
