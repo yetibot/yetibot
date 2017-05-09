6007ada36008346529f686eb0300046b4ff432c6
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-05-09 15:05:48 -0300

    Rewrite google tests with midje

--- test/yetibot/test/commands/google.clj
+++ test/yetibot/test/commands/google.clj
@@ -1,9 +1,8 @@
 (ns yetibot.test.commands.google
   (:require
-   [clojure.test :refer :all]
+   [midje.sweet :refer [fact => truthy anything has]]
    [yetibot.commands.google :as command]
-   [yetibot.api.google :as api]
-   ))
+   [yetibot.api.google :as api]))
 
 (defonce test-call-response
   {:items [{:title "x-files"
@@ -16,107 +15,93 @@
 
 (defonce no-results-response {:searchInformation {:totalResults "0"}})
 
-(deftest options-processor-invalid-input
+(fact options-processor-invalid-input
   (let [results (command/options-processor "jughead --nu 2")]
-    (is (seq (:errors results)))))
+    (seq (:errors results))) => truthy)
 
-(deftest options-processor-valid-input
+(fact options-processor-valid-input
   (let [results (command/options-processor "jughead jones --num 2")]
-    (is (and
-         (empty? (:errors results))
-         (= "jughead jones" (:query results))))))
+      (empty? (:errors results)) => true
+      (:query results) => "jughead jones"))
 
-(deftest search-function-input-validation-invalid-input
+(fact search-function-input-validation-invalid-input
   (let [results (command/search-function-input-validation
                  "jughead --nu 2")]
-    (is (= :failure (:status results)))))
+    (:status results) => :failure))
 
-(deftest search-function-input-validation-empty-query
+(fact search-function-input-validation-empty-query
   (let [results (command/search-function-input-validation "--num 2")]
-    (is (= :failure (:status results)))))
+    (:status results) => :failure))
 
-(deftest keyword->apikeyword-test
+(fact keyword->apikeyword-test
   (let [test-data (map (fn [[k v]] [k (:keyword v)])
                        api/accepted-keywords)]
-    (is (every? true?
-                (map (fn [[k v]]
-                       (= (command/keyword->apikeyword k) v))
-                     test-data)))))
+      (map (fn [[k v]] (= (command/keyword->apikeyword k) v)) test-data)) => (has every? true?))
 
-(deftest command-search-no-results-test
-  (with-redefs-fn
-    {#'api/search (fn [_ & _] no-results-response)}
-    #(is (= (command/search {:match ["" "something"]})
-            (:search command/messages)))))
+(fact command-search-no-results-test
+  (command/search {:match ["" "something"]}) => (:search command/messages)
+  (provided
+    (api/search anything :args anything) => no-results-response))
 
-(deftest command-image-search-no-results-test
-  (with-redefs-fn
-    {#'api/image-search (fn [_ & _] no-results-response)}
-    #(is (= (command/image-search {:match ["" "something"]})
-            (:image command/messages)))))
+(fact command-image-search-no-results-test
+  (command/image-search {:match ["" "something"]}) => (:image command/messages)
+  (provided
+    (api/image-search anything :args anything) => no-results-response))
 
-(deftest command-search-bad-response-test
-  (with-redefs-fn
-    {#'api/search (fn [_ & _] nil)}
-    #(is (= (command/search {:match ["" "something"]})
-            (:google-died command/messages)))))
+(fact command-search-bad-response-test
+  (command/search {:match ["" "something"]}) => (:google-died command/messages)
+  (provided
+    (api/search anything :args anything) => nil))
 
-(deftest command-image-search-bad-response-test
-  (with-redefs-fn
-    {#'api/image-search (fn [_ & _] nil)}
-    #(is (= (command/image-search {:match ["" "something"]})
-            (:google-died command/messages)))))
+(fact command-image-search-bad-response-test
+  (command/image-search {:match ["" "something"]}) => (:google-died command/messages)
+  (provided
+    (api/image-search anything :args anything) => nil))
 
-(deftest command-search-test-without-options
-  (with-redefs-fn
-    {#'api/search (fn [_ & _] test-call-response)}
-    #(is (= (command/search {:match ["" "something"]})
-            (api/format-results (:items test-call-response))))))
+(fact command-search-test-without-options
+  (command/search {:match ["" "something"]}) => (api/format-results (:items test-call-response))
+  (provided
+    (api/search anything :args anything) => test-call-response))
 
-(deftest command-image-search-test-without-options
-  (with-redefs-fn
-    {#'api/image-search (fn [_ & _] test-call-response)}
-    #(is (= (command/image-search {:match ["" "something"]})
-            (api/format-results (:items test-call-response) :order :image)))))
+(fact command-image-search-test-without-options
+  (command/image-search {:match ["" "something"]}) =>
+    (api/format-results (:items test-call-response) :order :image)
+  (provided
+    (api/image-search anything :args anything) => test-call-response))
 
-(deftest command-search-test-with-valid-options
-  (with-redefs-fn
-    {#'api/search (fn [_ & _] test-call-response)}
-    #(is (= (command/search {:match ["" "something --num 2 -y 3"]})
-            (api/format-results (:items test-call-response))))))
+(fact command-search-test-with-valid-options
+  (command/search {:match ["" "something --num 2 -y 3"]}) =>
+    (api/format-results (:items test-call-response))
+  (provided
+    (api/search anything :args anything) => test-call-response))
 
-(deftest command-image-search-test-with-valid-options
-  (with-redefs-fn
-    {#'api/image-search (fn [_ & _] test-call-response)}
-    #(is (= (command/image-search {:match ["" "something -t photo -O nothing"]})
-            (api/format-results (:items test-call-response) :order :image)))))
+(fact command-image-search-test-with-valid-options
+  (command/image-search {:match ["" "something -t photo -O nothing"]}) =>
+    (api/format-results (:items test-call-response) :order :image)
+  (provided
+    (api/image-search anything :args anything) => test-call-response))
 
-(deftest command-search-test-with-empty-query
-  (with-redefs-fn
-    {#'api/search (fn [_ & _] test-call-response)}
-    #(is (= (command/search {:match ["" " --num 2 -y 3"]})
-            (get command/messages :empty-query)))))
+(fact command-search-test-with-empty-query
+  (command/search {:match ["" " --num 2 -y 3"]}) =>
+    (get command/messages :empty-query))
 
-(deftest command-image-search-test-with-empty-query
-  (with-redefs-fn
-    {#'api/image-search (fn [_ & _] test-call-response)}
-    #(is (= (command/image-search {:match ["" " -t photo -O nothing"]})
-            (get command/messages :empty-query)))))
+(fact command-image-search-test-with-empty-query
+  (command/image-search {:match ["" " -t photo -O nothing"]}) =>
+    (get command/messages :empty-query))
 
-(deftest state-of-set-options-test
-  (with-redefs-fn
-    {#'command/options-atom (atom {:wilfred "demigod"})}
-    #(is (seq (command/state-of-set-options)))))
+(fact state-of-set-options-test
+  (with-redefs {#'command/options-atom (atom {:wilfred "demigod"})}
+    #(seq (command/state-of-set-options)) => truthy))
 
-(deftest empty-state-of-set-options-test
-  (is (empty? (command/state-of-set-options))))
+(fact empty-state-of-set-options-test
+  (command/state-of-set-options) => empty?)
 
-(deftest populating-atom-from-options-in-config
+(fact populating-atom-from-options-in-config
   (let [key :imgcolortype
         val "mono"
         keyword (get-in api/accepted-keywords [key :keyword])]
     (with-redefs-fn
       {#'command/options-atom (atom {})
        #'api/populate-options-from-config (fn [] {key val})}
-      #(is (= (do (command/load-options-from-file-into-atom)
+      #(clojure.test/is (= (do (command/load-options-from-file-into-atom)
                   @command/options-atom) {keyword val})))))
