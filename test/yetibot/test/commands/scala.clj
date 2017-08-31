95e63e8b1ef02b790463c46595d04695177b80ec
Trevor Hartman <trevorhartman@gmail.com>
2017-08-31 12:08:07 -0600

    Disable broken Scala tests until #665 is fixed

--- test/yetibot/test/commands/scala.clj
+++ test/yetibot/test/commands/scala.clj
@@ -6,17 +6,23 @@
 
 (def eval-and-extract (comp extract-result try-scala))
 
-(fact should-successfully-extract-valid-expression-test-scala
-  (eval-and-extract "1 + 1; 2 + 2") => '("2: Int" "4: Int"))
+(comment
+  ;; Scala API is broken. TODO Find a replacement:
+  ;; https://github.com/yetibot/yetibot/issues/665
 
-(fact should-return-not-found-for-unknown-variables
-  (eval-and-extract "a; b") => '("not found: value a" "not found: value b"))
+  (fact should-successfully-extract-valid-expression-test-scala
+    (eval-and-extract "1 + 1; 2 + 2") => '("2: Int" "4: Int"))
 
-(fact should-return-error-for-invalid-operations
-  (eval-and-extract "1 / 0") => '("java.lang.ArithmeticException: / by zero"))
+  (fact should-return-not-found-for-unknown-variables
+    (eval-and-extract "a; b") => '("not found: value a" "not found: value b"))
 
-(fact should-evaluate-properties
-  (eval-and-extract "scala.util.Properties.versionString") => '("\"version 2.11.8\": String"))
+  (fact should-return-error-for-invalid-operations
+    (eval-and-extract "1 / 0") => '("java.lang.ArithmeticException: / by zero"))
 
-(fact should-allow-evaluation-with-pipes
-  (parser "scala \"true || false\"") => truthy)
+  (fact should-evaluate-properties
+    (eval-and-extract "scala.util.Properties.versionString") => '("\"version 2.11.8\": String"))
+
+  (fact should-allow-evaluation-with-pipes
+    (parser "scala \"true || false\"") => truthy)
+
+  )
