fde37d237945787f7cb802ef8a1ad2026168ae99
Trevor Hartman <trevorhartman@gmail.com>
2017-09-05 07:11:47 -0600

    Set min-lein-version 2.0.0

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
+  :min-lein-version "2.0.0"
   :profiles {;; optionally override this profile in profiles.clj to be merged
              ;; into dev profile
              :profiles/dev {}
