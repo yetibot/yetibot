9f97de235aad424198163d124f5fc14e7cc21845
Trevor Hartman <trevorhartman@gmail.com>
2017-10-08 15:15:00 -0600

    Upgrade to yetibot.core 0.4.13

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  :timeout 120000
                  :welcome (println "Welcome to the yetibot development REPL!")}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [yetibot.core "0.4.11"]
+                 [yetibot.core "0.4.13"]
 
                  ; apis
                  [twitter-api "1.8.0"]
