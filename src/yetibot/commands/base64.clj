9d4f9d0ba44148939744d7239a3868bf272b6dc4
themistoklik <thpapavasileiou@gmail.com>
2017-10-26 21:01:17 +0300

    replaced into-bytes with native .getBytes

--- src/yetibot/commands/base64.clj
+++ src/yetibot/commands/base64.clj
@@ -1,26 +1,21 @@
-= (ns yetibot.commands.base64
-    (:require
-      [yetibot.core.hooks :refer [cmd-hook]]
-      [clojure.data.codec.base64 :as b64]))
-
-(defn into-bytes
-  "turns a string into a byte array"
-  [s]
-  (byte-array (map (comp byte int) s)))
+(ns yetibot.commands.base64
+  (:require
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [clojure.data.codec.base64 :as b64]))
 
 (defn encode-cmd
-  "base64 encode a string"
+  "base64 encode <string>"
   [{[_ s] :match}]
   (try
-    (String. (b64/encode (into-bytes s)))
+    (String. (b64/encode (.getBytes s)))
     (catch Exception _ "Oops! Can't encode that.")))
 
 
 (defn decode-cmd
-  "base64 decode a string"
+  "base64 decode <string>"
   [{[_ s] :match}]
   (try
-    (String. (b64/decode (into-bytes s)))
+    (String. (b64/decode (.getBytes s)))
     (catch Exception _ "Oops! Cant' decode that.")))
 
 (cmd-hook #"base64"
--- test/yetibot/test/commands/base64.clj
+++ test/yetibot/test/commands/base64.clj
@@ -6,19 +6,19 @@
 
 
 (fact test-encode-no-padding
-      (String. (b64/encode (into-bytes "any carnal pleasur"))) => "YW55IGNhcm5hbCBwbGVhc3Vy")
+      (String. (b64/encode (.getBytes "any carnal pleasur"))) => "YW55IGNhcm5hbCBwbGVhc3Vy")
 
 (fact test-encode-padding-1
-      (String. (b64/encode (into-bytes "any carnal pleasu"))) => "YW55IGNhcm5hbCBwbGVhc3U=")
+      (String. (b64/encode (.getBytes "any carnal pleasu"))) => "YW55IGNhcm5hbCBwbGVhc3U=")
 
 (fact test-encode-padding-2
-      (String. (b64/encode (into-bytes "any carnal pleas"))) => "YW55IGNhcm5hbCBwbGVhcw==")
+      (String. (b64/encode (.getBytes "any carnal pleas"))) => "YW55IGNhcm5hbCBwbGVhcw==")
 
 (fact test-decode-no-padding
-      (String. (b64/decode (into-bytes "YW55IGNhcm5hbCBwbGVhc3Vy"))) => "any carnal pleasur")
+      (String. (b64/decode (.getBytes "YW55IGNhcm5hbCBwbGVhc3Vy"))) => "any carnal pleasur")
 
 (fact test-decode-padding-1
-      (String. (b64/decode (into-bytes "YW55IGNhcm5hbCBwbGVhc3U="))) => "any carnal pleasu")
+      (String. (b64/decode (.getBytes "YW55IGNhcm5hbCBwbGVhc3U="))) => "any carnal pleasu")
 
 (fact test-decode-padding-2
-      (String. (b64/decode (into-bytes "YW55IGNhcm5hbCBwbGVhcw=="))) => "any carnal pleas")
+      (String. (b64/decode (.getBytes "YW55IGNhcm5hbCBwbGVhcw=="))) => "any carnal pleas")
