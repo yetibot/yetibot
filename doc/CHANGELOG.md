ca6f7d5d0ab12770a3b704d38aaf8d7c84ff1646
Trevor Hartman <trevorhartman@gmail.com>
2018-02-26 20:53:17 -0700

    Upgrade to yetibot.core 0.4.25

--- doc/CHANGELOG.md
+++ doc/CHANGELOG.md
@@ -6,6 +6,10 @@ which contains Yetibot's core functionality along with a few commands. See
 changelog](https://github.com/yetibot/yetibot.core/blob/master/doc/CHANGELOG.md)
 as well.
 
+## 0.4.27
+
+- Upgrade to yetibot.core 0.4.25
+
 ## 0.4.26
 
 - Upgrade to yetibot.core 0.4.24
--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                  :timeout 120000
                  :welcome (println "Welcome to the yetibot development REPL!")}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [yetibot.core "0.4.24"]
+                 [yetibot.core "0.4.25"]
 
                  ; apis
                  [twitter-api "1.8.0"]
