32d6e5da427424c7312dab8c6254f527dd2f24a8
Trevor Hartman <trevorhartman@gmail.com>
2018-10-15 11:18:28 -0600

    Version 0.4.50

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yetibot "0.4.50-SNAPSHOT"
+(defproject yetibot "0.4.50"
   :description "A command line in your chat, where chat ∈ {irc,slack}."
   :url "https://github.com/yetibot/yetibot"
   :license {:name "Eclipse Public License"
