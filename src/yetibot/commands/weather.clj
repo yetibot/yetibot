12ee460a093e16f685b37cc5e5c868915c28b8bb
Jereme Corrado <jereme@zoion.net>
2018-10-19 09:50:28 -0400

    WIP

--- src/yetibot/commands/weather.clj
+++ src/yetibot/commands/weather.clj
@@ -1,23 +1,30 @@
 (ns yetibot.commands.weather
   (:require
     [schema.core :as sch]
-    [clojure.string :refer [join]]
+    [clojure.string :as str]
     [yetibot.core.util.http :refer [get-json fetch encode map-to-query-string]]
     [taoensso.timbre :refer [info warn error]]
     [yetibot.core.config :refer [get-config]]
-    [yetibot.core.hooks :refer [cmd-hook]]))
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [yetibot.models.postal-code :refer [chk-postal-code]]))
 
 (def config (:value (get-config sch/Any [:weather :weatherbitio])))
 
 (def api-key (:key config))
 (def default-zip (-> config :default :zip))
 
-(defn endpoint [repr q]
-  (format "https://api.weatherbit.io/v2.0/%s?key=%s&units=I&%s" repr api-key q))
+(defn- endpoint-base [repr q]
+  (format "https://api.weatherbit.io/v2.0/%s?key=%s&%s" repr api-key q))
 
-(defn- current [loc]
-  (let [url (endpoint "current" (format "city=%s" (encode loc)))]
-    (get-json url)))
+(defn- current-by-name
+  "Get current conditions by location name str"
+  [name]
+  (get-json (endpoint "current" (format "city=%s" (encode name)))))
+
+(defn- current-by-pc
+  "Get current conditions by post code and country code"
+  [pc cc]
+  (get-json (endpoint "current" (apply format "postal_code=%s&country=%s" (map encode [pc cc])))))
 
 (defn- error-response [c] (:error c))
 
@@ -30,6 +37,12 @@
        (format "Wind: %s from the %s" (:wind_spd loc) (:wind_cdir_full loc))
        (format "Precip: %s" (or (:precip loc) "none"))
        ])))
+
+(defn current
+  [s]
+  (if-let [[pc cc] (apply chk-postal-code (str/split s #"\s*,\s*"))]
+    (current-by-pc pc cc)
+    (current-by-name s)))
 
 (defn weather-cmd
   "weather <location> # look up current weather for <location>"
