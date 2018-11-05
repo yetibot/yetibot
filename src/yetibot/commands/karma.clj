6be5db6388574ac909e1b4195627400bbd9eae04
Jereme Corrado <jereme@zoion.net>
2018-11-05 14:50:25 -0500

    Karma @username support POC: manifest as output formatter (#781)

--- src/yetibot/commands/karma.clj
+++ src/yetibot/commands/karma.clj
@@ -5,23 +5,34 @@
    [clojure.string :as str]
    [clj-time.format :as fmt]))
 
+(defn- format-output
+  [{{adapter :adapter} :chat-source} str]
+  (let [formatter (condp = adapter
+                    :slack #(str/replace % #"(@\w+)" "<$1>")
+                    identity)]
+    (formatter str)))
+
 (defn get-score
   "karma <user> # get score and recent notes for <user>"
   {:yb/cat #{:fun}}
-  [{user-id :match}]
-  (str (format "%s: %s\n" user-id (model/get-score user-id))
-       (str/join "\n" (map #(format "_\"%s\"_ --%s _(%s)_"
-                                    (:note %)
-                                    (:voter-id %)
-                                    (fmt/unparse (fmt/formatters :mysql) (:created-at %)))
-                           (model/get-notes user-id)))))
+  [{user-id :match :as ctx}]
+  (format-output
+   ctx
+   (str (format "%s: %s\n" user-id (model/get-score user-id))
+        (str/join "\n" (map #(format "_\"%s\"_ --%s _(%s)_"
+                                     (:note %)
+                                     (:voter-id %)
+                                     (fmt/unparse (fmt/formatters :mysql) (:created-at %)))
+                            (model/get-notes user-id))))))
 
 (defn get-high-scores
   "karma # get leaderboard"
   {:yb/cat #{:fun}}
-  [_]
-  (str/join "\n" (map #(format "%s: %s" (:user-id %) (:score %))
-                      (model/get-high-scores))))
+  [ctx]
+  (format-output
+   ctx
+   (str/join "\n" (map #(format "%s: %s" (:user-id %) (:score %))
+                       (model/get-high-scores)))))
 
 (defn adjust-score
   "karma <user>(++|--) <note> # adjust karma for <user> with optional <note>"
@@ -41,6 +52,6 @@
       "💔")))
 
 (cmd-hook ["karma" #"^karma$"]
-          #"(?x) (\w+) (--|\+\+) (?: \s+ (.+) )?" adjust-score
-          #"\w+" get-score
+          #"(?x) (@?\w+) \s{0,2} (--|\+\+) (?: \s+(.+) )?" adjust-score
+          #"@?\w+" get-score
           _ get-high-scores)
