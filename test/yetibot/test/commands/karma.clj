ec9056fadf392829c22e41256a5a8480927f8a28
Jereme Corrado <jereme@zoion.net>
2018-11-12 11:10:42 -0500

    Karma parsing now more selective; closes: #788

--- test/yetibot/test/commands/karma.clj
+++ test/yetibot/test/commands/karma.clj
@@ -14,29 +14,90 @@
 (def test-score 1000000)
 
 ;; Simulate the context passed to our command handlers
-(def slack-context {:chat-source {:adapter :slack}
-                    :match test-user})
+(def slack-context {:chat-source {:adapter :slack}, :user {:id test-voter :name test-voter}})
 
 (namespace-state-changes (before :contents (db/start)))
 
-(with-state-changes [(after :contents (model/delete-user! test-user))]
-
-  ;; Setup test user
-  (model/add-score-delta! test-user test-voter test-score test-note)
+(with-state-changes [(after :facts (model/delete-user! test-user))]
 
   ;; Our DB could have other users' scores so we make the assumption
   ;; that a high enough `test-score' will keep our `test-user' in the
   ;; leaderboard.
-  (fact "high score data includes our test-user and test-score"
-        (->> (get-high-scores slack-context)
-             :result/data
-             (filter #(= (:user-id %) test-user)))
-        => [{:user-id test-user :score test-score}])
+  (fact get-score
+        (model/add-score-delta! test-user test-voter test-score test-note)
+        (let [data (-> (get-score (assoc slack-context :match test-user))
+                       :result/data)]
+          (:user-id data)                  => test-user
+          (:score data)                    => test-score
+          (-> data :notes first :note)     => test-note
+          (-> data :notes first :voter-id) => test-voter))
 
-  (fact "user score includes note from voter"
-        (->> (get-score slack-context)
-             :result/data
-             ((fn [m] (update m :notes #(-> % first (dissoc :created-at) vector)))))
-        => {:user-id test-user
-            :score test-score
-            :notes [{:note test-note :voter-id test-voter}]}))
+  (fact get-high-score
+        (model/add-score-delta! test-user test-voter test-score test-note)
+        (let [data (->> (get-high-scores slack-context)
+                        :result/data
+                        (filter #(= (:user-id %) test-user))
+                        first)]
+          (:user-id data) => test-user
+          (:score data)   => test-score))
+
+  (fact cmp-user-ids
+        (cmp-user-ids "@foo" "@foo")     => truthy
+        (cmp-user-ids "foo" "foo")       => truthy
+        (cmp-user-ids "@foo" "foo")      => truthy
+        (cmp-user-ids "@foo" "@bar") =not=> truthy
+        (cmp-user-ids "foo" "bar")   =not=> truthy
+        (cmp-user-ids "@foo" "bar")  =not=> truthy)
+
+  (fact "adjust-score can increase another users karma"
+        (let [data (-> (adjust-score (assoc slack-context :match (format "%s++" test-user)))
+                       :result/data)]
+          (:user-id data) => test-user
+          (:score data)   => 1))
+
+  (fact "adjust-score can increase another users karma and include a note"
+        (let [data (-> (adjust-score (assoc slack-context :match (format "%s++ %s" test-user test-note)))
+                       :result/data)]
+          (:user-id data)               => test-user
+          (:score data)                 => 1
+          (-> data :notes first :note)     => test-note
+          (-> data :notes first :voter-id) => test-voter))
+
+  (fact "adjust-score can decrease another users karma"
+        (let [data (-> (adjust-score (assoc slack-context :match (format "%s--" test-user)))
+                       :result/data)]
+          (:user-id data) => test-user
+          (:score data)   => -1))
+
+  (fact "adjust-score can decrease another users karma and include a note"
+        (let [data (-> (adjust-score (assoc slack-context :match (format "%s-- %s" test-user test-note)))
+                       :result/data)]
+          ;; We don't return notes for negative karma votes
+          (:user-id data) => test-user
+          (:score data)   => -1))
+
+  (fact "adjust-score allows a user to decrease their own karma"
+        (let [data (-> (adjust-score (-> slack-context
+                                         (assoc :match (format "%s--" test-user))
+                                         (assoc-in [:user :id] test-user)))
+                       :result/data)]
+          (:user-id data) => test-user
+          (:score data)   => -1))
+
+  (fact "adjust-score precludes a user from increasing their own karma"
+        (let [r (adjust-score (-> slack-context
+                                  (assoc :match (format "%s++" test-user))
+                                  (assoc-in [:user :id] test-user)))]
+          (:result/error r) => truthy))
+
+  (fact "adjust-score tightly parses invocations"
+        (adjust-score (assoc slack-context :match (format "%s+-" test-user)))   => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "+-%s" test-user)))   => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "++%s" test-user)))   => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "--%s" test-user)))   => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "++%s++" test-user))) => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "++%s--" test-user))) => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "--%s--" test-user))) => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "--%s++" test-user))) => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "%s++--" test-user))) => #(contains? % :result/error)
+        (adjust-score (assoc slack-context :match (format "%s--++" test-user))) => #(contains? % :result/error)))
