4aed68f88d8ea955ac780457613baa3e8f663aba
Jereme Corrado <jereme@zoion.net>
2018-11-15 07:30:44 -0500

    POC: create and alias sub-ns to avoid long ns-qualified kw for specs

--- src/yetibot/commands/karma/specs.clj
+++ src/yetibot/commands/karma/specs.clj
@@ -1,6 +1,11 @@
 (ns yetibot.commands.karma.specs
   (:require [clojure.spec.alpha :as s]))
 
+;; testing an idea
+(defn mk-subns-alias!
+  [sym]
+  (alias sym (-> (ns-name *ns*) (str "." sym) symbol create-ns)))
+
 ;; Common specs we could relocatexo
 (s/def ::name string?)
 (s/def ::id string?)
@@ -20,15 +25,17 @@
 ;; run-time fn data validators
 
 ;; get-score
-(s/def :yetibot.commands.karma.specs.get-score/match string?)
+(mk-subns-alias! 'get-score)
+(s/def ::get-score/match string?)
 (s/def ::get-score-ctx
-  (s/merge ::ctx (s/keys :req-un [:yetibot.commands.karma.specs.get-score/match])))
+  (s/merge ::ctx (s/keys :req-un [::get-score/match])))
 
 
 ;; adjust-score
-(s/def :yetibot.commands.karma.specs.adjust-score/match (s/and vector?
-                                                               (s/cat :user-id ::user-id
-                                                                      :action ::action
-                                                                      :note (s/? ::note))))
+(mk-subns-alias! 'adjust-score)
+(s/def ::adjust-score/match (s/and vector?
+                                   (s/cat :user-id ::user-id
+                                          :action ::action
+                                          :note (s/? ::note))))
 (s/def ::adjust-score-ctx
-  (s/merge ::ctx (s/keys :req-un [:yetibot.commands.karma.specs.adjust-score/match])))
+  (s/merge ::ctx (s/keys :req-un [::adjust-score/match])))
