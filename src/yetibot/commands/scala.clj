035e682993e1a1efb889f101bbf084abebb818d7
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2018-11-28 23:38:26 +0100

    Fix Scala evaluation command

--- src/yetibot/commands/scala.clj
+++ src/yetibot/commands/scala.clj
@@ -4,51 +4,105 @@
     [yetibot.core.hooks :refer [cmd-hook]]
     [clj-http.client :as client]
     [clojure.string :as s]
-    [cheshire.core :as json]))
+    [cheshire.core :as json]
+    [kvlt.core :as kvlt]
+    [clojure.core.async :as async]))
 
-(def endpoint "http://www.scalakata.com/api/com/scalakata/Api/eval")
+(def endpoint "https://scastie.scala-lang.org/api/")
 
-(defn make-body [scala-str]
-  {:code
-   (str
-     "import com.scalakata._; @instrument class Playground {"
-     scala-str
-     "}")})
+(def sbt-config-extra "scalacOptions ++= Seq(\"-deprecation\",  \"-encoding\", \"UTF-8\",  \"-feature\",  \"-unchecked\")")
+
+(def scala-version "2.12.6")
+
+(def platform "Jvm")
+
+(defn make-body
+  [scala-str]
+  (json/generate-string {:_isWorksheetMode       true
+                         :code                   scala-str
+                         :target                 {:scalaVersion scala-version
+                                                  :$type        platform}
+                         :libraries              []
+                         :librariesFromList      []
+                         :sbtConfigExtra         sbt-config-extra
+                         :sbtPluginsConfigExtra  ""
+                         :isShowingInUserProfile false}))
 
 (defn try-scala
+  "Submits the Scala expression to the evaluation API and returns a
+  Server-Sent Events core.async channel for retrieving the result."
   [expr]
   (info "scala:" expr)
-  (client/post
-    endpoint
-    {:body
-     (json/generate-string {:request (json/generate-string (make-body expr))})
-     :as :json}))
+  (as-> (str endpoint "run") v
+        (client/post v {:content-type :json
+                        :accept       :json
+                        :body         (make-body expr)})
+        (:body v)
+        (json/parse-string v true)
+        (:base64UUID v)
+        (str endpoint "progress-sse/" v)
+        (kvlt/event-source! v)))
 
-(defn extract-success [body]
-  (let [instrs (:instrumentation body)]
-    (map
-      (fn [[coords {:keys [className v]}]] (format "%s: %s" v className))
-      instrs)))
+(defn extract-success
+  "Extracts success evaluation info from the SSE-message data."
+  [{user-output :userOutput instrumentations :instrumentations}]
+  (cond
+    (seq user-output) (get-in user-output [:line])
+    (seq instrumentations) (->> instrumentations
+                                (map (fn [{{v :v className :className} :render}]
+                                       (format "%s: %s" v className)))
+                                (s/join "\n"))))
 
-(defn extract-result [resp]
-  (let [body (:body resp)]
-    (cond
-      ;; timeout
-      (:timeout body) (str "The expression timed out")
-      ;; runtime error(s)
-      (seq (:runtimeError body)) (map :message (:runtimeError body))
-      ;; compilation error(s)
-      (seq (:complilationInfos body)) (mapcat
-                                        (fn [[_ msgs]] (map :message msgs))
-                                        (:complilationInfos body))
-      ;; success
-      :else (extract-success body))))
+(defmulti format-result
+          "Returns a dispatch-value based on the SSE data received from the evaluation API."
+          (fn [{:keys [compilationInfos isTimeout isDone]}]
+            (cond
+              (true? isTimeout) :eval-timeout
+              (seq compilationInfos) :eval-runtime-error
+              (true? isDone) :is-done
+              :else :eval-success)))
+
+(defmethod format-result :eval-timeout
+  [_]
+  (str "The expression timed out"))
+
+(defmethod format-result :eval-runtime-error
+  [data]
+  (->> data
+       :compilationInfos
+       (map :message)
+       (s/join "\n")))
+
+(defmethod format-result :is-done
+  [_]
+  :is-done)
+
+(defmethod format-result :eval-success
+  [data]
+  (extract-success data))
+
+(defn extract-result
+  "Extracts the Scala evaluation result from the core.async channel provided
+   as argument."
+  [chan]
+  (async/go-loop [results []]
+    (let [result (as-> (async/<! chan) v
+                       (json/parse-string (:data v) true)
+                       (format-result v))]
+      (if (= :is-done result)
+        (do
+          (async/close! chan)
+          (->> results
+               (remove nil?)
+               (s/join "\n")))
+        (recur (conj results result))))))
 
 (defn scala-cmd
   "scala <expression> # evaluate a scala expression"
   {:yb/cat #{:repl}}
   [{expr :match}]
-  (extract-result (try-scala expr)))
+  (async/<!!
+    (extract-result (try-scala expr))))
 
 (cmd-hook #"scala"
           #".*" scala-cmd)
