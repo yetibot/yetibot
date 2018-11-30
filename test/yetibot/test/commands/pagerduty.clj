e454013fce217581e56a6787040f28b0ee7bec35
Trevor Hartman <trevorhartman@gmail.com>
2018-11-30 13:10:34 -0700

    Add more pagerduty tests

--- test/yetibot/test/commands/pagerduty.clj
+++ test/yetibot/test/commands/pagerduty.clj
@@ -1,13 +1,40 @@
 (ns yetibot.test.commands.pagerduty
   (:require
-   [midje.sweet :refer [namespace-state-changes with-state-changes fact => truthy]]
+   [midje.sweet :refer [fact =>]]
    [yetibot.core.util.command-info :refer [command-execution-info]]
    [yetibot.commands.pagerduty :refer :all]))
 
 (fact
   "teams show subcommand matches"
-  (let [{:keys [match matched-sub-cmd]} (command-execution-info
-                                          "pagerduty teams show foo")]
+  (let [{:keys [match matched-sub-cmd]}
+        (command-execution-info "pagerduty teams show foo")]
     (last match) => "foo"
-    matched-sub-cmd => #'teams-show-cmd
-    ))
+    matched-sub-cmd => #'teams-show-cmd))
+
+(fact
+  "teams matches the correct command"
+  (let [{:keys [match matched-sub-cmd]}
+        (command-execution-info "pagerduty teams foo")]
+    (first match) => "teams foo"
+    matched-sub-cmd => #'teams-cmd))
+
+(fact
+  "users matches the correct command"
+  (let [{:keys [match matched-sub-cmd]}
+        (command-execution-info "pagerduty users foo")]
+    (first match) => "users foo"
+    matched-sub-cmd => #'users-cmd))
+
+(fact
+  "schedules matches the correct command"
+  (let [{:keys [match matched-sub-cmd]}
+        (command-execution-info "pagerduty schedules foo")]
+    (first match) => "schedules foo"
+    matched-sub-cmd => #'schedules-cmd))
+
+(fact
+  "schedules show matches the correct command"
+  (let [{:keys [match matched-sub-cmd]}
+        (command-execution-info "pagerduty schedules show foo")]
+    (first match) => "schedules show foo"
+    matched-sub-cmd => #'schedules-show-cmd))
