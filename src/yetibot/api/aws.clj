bc36896e2caa0726883d2b880cf25c18bec82d29
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2019-01-09 18:20:16 +0100

    Add command and api namespaces for AWS
    The configuration is validated with Schema instead of Specs since using this latter involves modifying 'get-config'. There will probably be another function to be added for specs support at some point.

---
+++ src/yetibot/api/aws.clj
@@ -0,0 +1,17 @@
+(ns yetibot.api.aws
+  (:require
+    [yetibot.core.schema :refer [non-empty-str]]
+    [schema.core :as sch]
+    [yetibot.core.config :refer [get-config]]))
+
+(def aws-schema
+  {:aws-access-key-id non-empty-str
+   :aws-secret-access-key non-empty-str
+   (sch/optional-key :region) non-empty-str})
+
+(defn config
+  "Returns AWS-related configuration"
+  []
+  (:value (get-config aws-schema [:aws])))
+
+(defn configured? [] (config))
