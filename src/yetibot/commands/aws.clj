957fd24e6f7075c34cded449f3deae58469d7819
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2019-01-15 00:57:46 +0100

    Fix aws implementation

--- src/yetibot/commands/aws.clj
+++ src/yetibot/commands/aws.clj
@@ -1,4 +1,15 @@
 (ns yetibot.commands.aws
   (:require
     [taoensso.timbre :refer [info]]
-    [yetibot.core.hooks :refer [cmd-hook]]))
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [yetibot.api.aws :as aws]))
+
+(defn echo-test
+      "Returns an echo for testing purpose"
+      []
+      (format "hey dude what's up!"))
+
+(when (aws/configured?)
+      (cmd-hook ["aws" #"aws"]
+                #"echo" echo-test))
+
