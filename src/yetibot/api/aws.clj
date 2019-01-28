9f9f572bcfe6e7ab6a1223c89e625b8d3e0027b4
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2019-01-28 01:03:02 +0100

    Add utility function for aws client creation

--- src/yetibot/api/aws.clj
+++ src/yetibot/api/aws.clj
@@ -5,9 +5,6 @@
     [yetibot.core.config :refer [get-config]]
     [cognitect.aws.client.api :as aws]))
 
-; AWS clients
-(def iam (aws/client {:api :iam}))
-
 (def aws-schema
   {:aws-access-key-id non-empty-str
    :aws-secret-access-key non-empty-str
@@ -17,5 +14,18 @@
   "Returns AWS-related configuration"
   []
   (:value (get-config aws-schema [:aws])))
+
+(def aws-access-key-id (:aws-access-key-id (config)))
+(def aws-secret-access-key (:aws-secret-access-key (config)))
+
+(defn make-aws-client
+  "Returns a aws client, given an aws service"
+  [service-name]
+  (aws/client {:api service-name
+               :credentials-provider (cognitect.aws.credentials/basic-credentials-provider
+                                       {:access-key-id aws-access-key-id
+                                        :secret-access-key aws-secret-access-key})}))
+; AWS clients
+(def iam (make-aws-client :iam))
 
 (defn configured? [] (config))
