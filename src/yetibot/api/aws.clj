819be8ac57a7e2fe0b0195ece14673ac86284635
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2019-01-29 23:49:05 +0100

    Implement generic aws result handling and aws iam group creation

--- src/yetibot/api/aws.clj
+++ src/yetibot/api/aws.clj
@@ -6,8 +6,8 @@
     [cognitect.aws.client.api :as aws]))
 
 (def aws-schema
-  {:aws-access-key-id non-empty-str
-   :aws-secret-access-key non-empty-str
+  {:aws-access-key-id         non-empty-str
+   :aws-secret-access-key     non-empty-str
    (sch/optional-key :region) non-empty-str})
 
 (defn config
@@ -15,17 +15,41 @@
   []
   (:value (get-config aws-schema [:aws])))
 
+(defn configured? [] (config))
+
+; aws API credentials
 (def aws-access-key-id (:aws-access-key-id (config)))
 (def aws-secret-access-key (:aws-secret-access-key (config)))
 
 (defn make-aws-client
-  "Returns a aws client, given an aws service"
+  "Returns an aws client given an aws keywordized service name (:iam, :ec2, :s3 etc.)"
   [service-name]
-  (aws/client {:api service-name
+  (aws/client {:api                  service-name
                :credentials-provider (cognitect.aws.credentials/basic-credentials-provider
-                                       {:access-key-id aws-access-key-id
+                                       {:access-key-id     aws-access-key-id
                                         :secret-access-key aws-secret-access-key})}))
 ; AWS clients
 (def iam (make-aws-client :iam))
 
-(defn configured? [] (config))
+(defmulti format-result
+          "Returns a dispatch-value based on the response received from the aws service"
+          (fn [{:keys [ErrorResponse]}]
+            (cond
+              (not (nil? ErrorResponse)) :error
+              :else :success)))
+
+(defmethod format-result :error
+  [result]
+  {:result/error (get-in result [:ErrorResponse :Error :Message])})
+
+(defmethod format-result :success
+  [{{:keys [Path GroupName GroupId Arn CreateDate]} :Group} result]
+  (format "Group %s/%s with Id %s and Arn %s has been created successfully on %s"
+          Path GroupName GroupId Arn CreateDate))
+
+(defn iam-create-group
+  "Creates an aws IAM group"
+  [group-name]
+  (let [response (aws/invoke iam {:op      :CreateGroup
+                                  :request {:GroupName group-name}})]
+    (format-result response)))
