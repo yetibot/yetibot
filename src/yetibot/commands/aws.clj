f44db63da67fe9a2815fe4fb8b3fd0fe1b33070c
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2019-02-09 22:33:15 +0100

    Implement aws iam delete-user command

--- src/yetibot/commands/aws.clj
+++ src/yetibot/commands/aws.clj
@@ -26,15 +26,21 @@
   (aws/iam-get-group group-name))
 
 (defn iam-list-groups-cmd
-  "aws iam list-groups <path-prefix># Lists the IAM groups that have the specified path prefix <path-prefix>"
+  "aws iam list-groups <path-prefix> # Lists the IAM groups that have the specified path prefix <path-prefix>"
   [{[_ path] :match}]
   (aws/iam-list-groups path))
+
+(defn iam-delete-user-cmd
+  "aws iam delete-user <user-name> # Deletes the specified IAM user. The user must not belong to any groups or have any access keys, signing certificates, or attached policies."
+  [{[_ user-name] :match}]
+  (aws/iam-delete-user user-name))
 
 (when (aws/configured?)
   (cmd-hook #"aws"
             #"iam create-group\s+(\S+)" iam-create-group-cmd
             #"iam list-groups\s+(\S+)" iam-list-groups-cmd
             #"iam create-user\s+(\S+)" iam-create-user-cmd
+            #"iam delete-user\s+(\S+)" iam-delete-user-cmd
             #"iam add-user-to-group\s+(\S+)\s+(\S+)" iam-add-user-to-group-cmd
             #"iam get-group\s+(\S+)" iam-get-group-cmd))
 
