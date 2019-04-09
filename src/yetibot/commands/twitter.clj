c8a6cf1a7666f0e492caa8691151c7d58cd3a239
Trevor Hartman <trevorhartman@gmail.com>
2019-04-08 20:41:06 -0600

    Show full text from Twitter and add data to results (#923)
    * Show full text from Twitter and add data to results
    
    * Return already tracking as error
    
    * Begin changelog

--- src/yetibot/commands/twitter.clj
+++ src/yetibot/commands/twitter.clj
@@ -12,7 +12,8 @@
   "twitter lookup <screen-name> # look up info on Twitter user"
   [{[_ screen-name] :match}]
   (let [user (-> (model/user-timeline screen-name) :body first :user)]
-    ((juxt :description :profile_image_url :location) user)))
+    {:result/data user
+     :result/value ((juxt :description :profile_image_url :location) user)}))
 
 (defn tweet
   "twitter tweet <status> # post <status> to Twitter"
@@ -23,69 +24,70 @@
   "twitter following # list Twitter users you are following"
   [_]
   (let [users (model/following)]
-    (map :screen_name users)))
+    {:result/data users
+     :result/value (map :screen_name users)}))
 
 (defn follow
   "twitter follow <screen-name> # follow a Twitter user"
   [{[_ screen-name] :match}]
   (let [body (:body (model/follow screen-name))
         [img desc] ((juxt :profile_image_url :description) body)]
-    [(str "Followed " screen-name ": " desc)
-     img]))
+    {:result/data body
+     :result/value [(str "Followed " screen-name ": " desc)
+                    img]}))
 
 (defn unfollow
   "twitter unfollow <screen-name> # stop following a Twitter user"
   [{[_ screen-name] :match}]
-  (model/unfollow screen-name)
-  (str "Unfollowed " screen-name))
+  {:result/data (model/unfollow screen-name)
+   :result/value (str "Unfollowed " screen-name)})
 
 (defn tracking
   "twitter tracking # show the topics that are being tracked on Twitter"
-  [_] (let [topics (map :topic (model/find-all))]
-        (if (empty? topics)
-          "You're not tracking anything yet."
-          topics)))
+  [_]
+  (let [topics (model/find-all)]
+    (if (empty? topics)
+      "You're not tracking anything yet."
+      {:result/data topics
+       :result/value (map :topic topics)})))
 
 (defn track
   "twitter track <topic> # track a <topic> on the Twitter stream"
   [{[_ topic] :match user :user}]
   (if (model/find-by-topic topic)
-    (format "You're already tracking %s." topic)
-    (do
-      (model/add-topic (:id user) topic)
-      (format "Tracking %s" topic))))
+    {:result/error (format "You're already tracking %s." topic)}
+    {:result/data (model/add-topic (:id user) topic)
+     :result/value (format "Tracking %s" topic)}))
 
 (defn untrack
   "twitter untrack <topic> # stop tracking <topic>"
   [{[_ topic] :match}]
   (if-let [{topic-id :id} (first (model/find-by-topic topic))]
-    (do
-      (model/remove-topic topic-id)
-      (format "Stopped tracking %s" topic))
-    (format "You're not tracking %s" topic)))
+    {:result/data (model/remove-topic topic-id)
+     :result/value (format "Stopped tracking %s" topic)}
+    {:result/error (format "You're not tracking %s" topic)}))
 
 (defn show
   "twitter show <screen-name> # show top 10 tweets from user <scree-name>"
   [{[_ screen-name] :match}]
   (let [tweets (:body (model/user-timeline screen-name 10))]
-    (map model/format-tweet-text tweets)))
+    {:result/data tweets
+     :result/value (map model/format-tweet-text tweets)}))
 
 (defn display
   "twitter display <id> # display tweet with <id>"
   [{[_ id] :match}]
-  (-> id
-      (model/show)
-      (get :body)
-      (model/format-tweet)))
+  (let [{body :body} (model/show id)]
+    {:result/data body
+     :result/value (model/format-tweet body)}))
 
 (defn search
   "twitter search <query> # find most recent 20 tweets matching <query>"
   {:yb/cat #{:info}}
   [{[_ query] :match}]
-  (->> (model/search query)
-       :body
-       :statuses
-       (map model/format-tweet)))
+  (let [{{statuses :statuses} :body} (model/search query)]
+    {:result/data statuses
+     :result/value (map model/format-tweet statuses)}))
 
 (defn retweet
   "twitter retweet <id>"
