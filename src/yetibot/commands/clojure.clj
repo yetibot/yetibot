45e0543ab5010e4ed0a6c053af6afcc634a1fd0a
Trevor Hartman <trevorhartman@gmail.com>
2019-04-11 15:42:44 -0600

    Upgrade clojail to fork and use simpler sandbox (#926)
    * Upgrade clojail to fork and use simpler sandbox
    
    * Allow clj to tap into piped data
    
    Fix https://github.com/yetibot/yetibot/issues/924
    
    * Add test for accessing data from clj command

--- src/yetibot/commands/clojure.clj
+++ src/yetibot/commands/clojure.clj
@@ -1,23 +1,51 @@
 (ns yetibot.commands.clojure
   (:require
     [clojure.string :as s]
-    [clojail.core :refer [sandbox]]
-    [clojail.testers :refer [secure-tester]]
+    [clojail.core :refer [sandbox safe-read]]
     [yetibot.core.hooks :refer [cmd-hook]]
     [taoensso.timbre :refer [error debug info color-str]]
     [yetibot.core.util.http :refer [get-json map-to-query-string]]))
 
-(def sb (sandbox secure-tester :timeout 5000))
-
 (defn clojure-cmd
   "clj <expression> # evaluate a clojure expression"
   {:yb/cat #{:util}}
-  [{:keys [args]}]
+  [{:keys [args data]}]
   (try
-    (pr-str (sb (read-string args)))
+    ;; create a sandbox with data from pipe available
+    (let [sb (sandbox [] :init `(def ~'data ~data))
+          result (sb (safe-read args))]
+      {:result/data result
+       :result/value (pr-str result)})
     (catch Throwable e
       (info "Clojail erroed" e)
-      (throw e) e)))
+      (throw e)
+      e)))
 
 (cmd-hook #"clj"
           #"\S*" clojure-cmd)
+
+(comment
+
+  (def sb (sandbox [] :timeout 5000))
+
+  (def data {:foo [1 2 3]})
+
+  (sb data)
+
+  (sb (:foo data))
+
+  (let [input-code "(reduce + (range 30))"]
+    '(read-string #'input-code)
+    )
+
+  (let [data {:foo [1]}
+        args "1"]
+      `(let [~'data ~data]
+         (eval (safe-read ~args))))
+
+  (let [data {:foo [1]}
+        args "data"
+        sb (sandbox [] :init `(def ~'data ~data))]
+    (sb (safe-read args)))
+
+  )
