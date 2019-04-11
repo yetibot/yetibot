d19431423aa622f94a3ad093aa4a05cb7cd83688
Trevor Hartman <trevorhartman@gmail.com>
2019-04-11 15:30:42 -0600

    Add test for accessing data from clj command

--- test/yetibot/test/commands/clojure.clj
+++ test/yetibot/test/commands/clojure.clj
@@ -1,11 +1,15 @@
 (ns yetibot.test.commands.clojure
   (:require
-    [midje.sweet :refer [fact => anything throws]]
-    [yetibot.commands.clojure :refer :all]
-    ))
+   [yetibot.core.midje :refer [value data]]
+   [midje.sweet :refer [fact => anything throws]]
+   [yetibot.commands.clojure :refer :all]))
 
 (fact "should eval as clojure code and return response using clojail"
-  (clojure-cmd {:args "(* 10 2)"}) => "20")
+  (clojure-cmd {:args "(* 10 2)"}) => (value "20"))
 
 (fact "should return error message when request fail"
   (clojure-cmd {:args "(+ 1 B)"}) => (throws Exception))
+
+(fact "should allow access to data"
+  (clojure-cmd {:args "(:location data)"
+                :data {:location "Montana"}}) => (data "Montana"))
