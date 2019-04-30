75603cb41e1c656a2f09d18ee260f51b681d1926
Trevor Hartman <trevorhartman@gmail.com>
2019-04-30 16:51:49 -0600

    Fix clj data with nested seqs (#945)
    * Add failing test for clj with nested seqs
    
    * Use inline data structure for clj test
    
    * Switch to dynamic var binding for clj access to data
    
    This avoids serialization issues reported in
    https://github.com/yetibot/yetibot/issues/943
    
    * Prevent accessing Yetibot namespaces in clojure command
    
    * Make exception checkers more general
    
    * Remove unused test resource-paths
    
    * Clean up namespaces

--- src/yetibot/commands/clojure.clj
+++ src/yetibot/commands/clojure.clj
@@ -1,10 +1,19 @@
 (ns yetibot.commands.clojure
-  (:require
-    [clojure.string :as s]
-    [clojail.core :refer [sandbox safe-read]]
-    [yetibot.core.hooks :refer [cmd-hook]]
-    [taoensso.timbre :refer [error debug info color-str]]
-    [yetibot.core.util.http :refer [get-json map-to-query-string]]))
+  (:require [clojail.core :refer [safe-read sandbox]]
+            [clojail.testers :refer [blacklist-nses secure-tester]]
+            [taoensso.timbre :refer [info]]
+            [yetibot.core.hooks :refer [cmd-hook]]
+            [yetibot.core.loader :refer [find-namespaces]]))
+
+(def
+  ^{:doc "Dynamic var to bind data into the Clojail sandbox"
+    :dynamic true}
+  *current-data* nil)
+
+(def sandbox-tester
+  (conj secure-tester
+        ;; disallow poking at internal Yetibot stuff
+        (blacklist-nses (find-namespaces #"yetibot\..+$"))))
 
 (defn clojure-cmd
   "clj <expression> # evaluate a clojure expression"
@@ -17,7 +26,10 @@
           ;; clojure.lang.PersistentHashMap
           ;; so convert it to a vector before serializing it into Clojail
           data (if (sequential? data) (vec data) data)
-          sb (sandbox [] :init `(def ~'data ~data))
+          sb (binding [*current-data* data]
+               (sandbox sandbox-tester
+                        :init `(def ~'data
+                                    yetibot.commands.clojure/*current-data*)))
           result (sb (safe-read args))]
       {:result/data result
        :result/value (pr-str result)})
@@ -39,9 +51,29 @@
 
   (sb (:foo data))
 
-  (let [data {:foo [1]}
-        args "1"]
-    `(let [~'data ~data]
-       (eval (safe-read ~args))))
+  (let [args "(count data)"
+        _ (prn (count data))
+        sb (sandbox [] :init `(def ~'data ~data))]
+    (sb (safe-read args)))
 
-)
+  ;;
+  (def sample-data
+    {:id 1234,
+     :members
+     '({:id 1001, :tid 2001}
+       {:id 1002, :tid 2002}
+       {:id 1003, :tid 2003}
+       {:id 1004, :tid 2004}
+       {:id 1005, :tid 2005}
+       {:id 1006, :tid 2006}
+       {:id 3001, :tid 4001}
+       {:id 3002, :tid 4002}
+       {:id 3003, :tid 4003}
+       {:id 3004, :tid 4004}
+       {:id 3005, :tid 4005}
+       {:id 3006, :tid 4006})})
+
+  (binding [*current-data* sample-data]
+    (let [args "(:members data)"
+          sb (sandbox [] :init `(def ~'data yetibot.commands.clojure/*current-data*))]
+      (sb (safe-read args)))))
--- test/yetibot/test/commands/clojure.clj
+++ test/yetibot/test/commands/clojure.clj
@@ -1,8 +1,23 @@
 (ns yetibot.test.commands.clojure
-  (:require
-   [yetibot.core.midje :refer [value data]]
-   [midje.sweet :refer [fact => anything throws]]
-   [yetibot.commands.clojure :refer :all]))
+  (:require [midje.sweet :refer [=> fact facts throws]]
+            [yetibot.commands.clojure :refer :all]
+            [yetibot.core.midje :refer [data value]]))
+
+(def sample-data
+  {:id 1234,
+   :members
+   '({:id 1001, :tid 2001}
+     {:id 1002, :tid 2002}
+     {:id 1003, :tid 2003}
+     {:id 1004, :tid 2004}
+     {:id 1005, :tid 2005}
+     {:id 1006, :tid 2006}
+     {:id 3001, :tid 4001}
+     {:id 3002, :tid 4002}
+     {:id 3003, :tid 4003}
+     {:id 3004, :tid 4004}
+     {:id 3005, :tid 4005}
+     {:id 3006, :tid 4006})})
 
 (fact "should eval as clojure code and return response using clojail"
   (clojure-cmd {:args "(* 10 2)"}) => (value "20"))
@@ -13,3 +28,14 @@
 (fact "should allow access to data"
   (clojure-cmd {:args "(:location data)"
                 :data {:location "Montana"}}) => (data "Montana"))
+
+(fact "should work with nested seq data structures"
+  (clojure-cmd {:args "(count (:members data))"
+                :data sample-data}) => (value "12"))
+
+(facts "should not be able to access outer Yetibot context"
+  (clojure-cmd
+    {:args "(clojail.testers/blanket \"foo\")"}) => (throws Exception)
+  (clojure-cmd
+    {:args "(yetibot.core.adapters.init/adapters-config)"}) => (throws Exception)
+  (clojure-cmd {:args "(yetibot.api.github/config)"}) => (throws Exception))
