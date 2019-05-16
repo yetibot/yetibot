33a9ea28099490e0533a9d93e8323eb737bf097b
Trevor Hartman <trevorhartman@gmail.com>
2019-05-16 12:17:54 -0700

    Test whitespace in pagerduty

--- test/yetibot/test/commands/pagerduty.clj
+++ test/yetibot/test/commands/pagerduty.clj
@@ -7,34 +7,34 @@
 (fact
   "teams show subcommand matches"
   (let [{:keys [match matched-sub-cmd]}
-        (command-execution-info "pagerduty teams show foo")]
-    (last match) => "foo"
+        (command-execution-info "pagerduty teams show foo bar")]
+    (last match) => "foo bar"
     matched-sub-cmd => #'teams-show-cmd))
 
 (fact
   "teams matches the correct command"
   (let [{:keys [match matched-sub-cmd]}
-        (command-execution-info "pagerduty teams foo")]
-    (first match) => "teams foo"
+        (command-execution-info "pagerduty teams foo bar")]
+    (first match) => "teams foo bar"
     matched-sub-cmd => #'teams-cmd))
 
 (fact
   "users matches the correct command"
   (let [{:keys [match matched-sub-cmd]}
-        (command-execution-info "pagerduty users foo")]
-    (first match) => "users foo"
+        (command-execution-info "pagerduty users foo bar")]
+    (first match) => "users foo bar"
     matched-sub-cmd => #'users-cmd))
 
 (fact
   "schedules matches the correct command"
   (let [{:keys [match matched-sub-cmd]}
-        (command-execution-info "pagerduty schedules foo")]
-    (first match) => "schedules foo"
+        (command-execution-info "pagerduty schedules foo bar")]
+    (first match) => "schedules foo bar"
     matched-sub-cmd => #'schedules-cmd))
 
 (fact
   "schedules show matches the correct command"
   (let [{:keys [match matched-sub-cmd]}
-        (command-execution-info "pagerduty schedules show foo")]
-    (first match) => "schedules show foo"
+        (command-execution-info "pagerduty schedules show foo bar")]
+    (first match) => "schedules show foo bar"
     matched-sub-cmd => #'schedules-show-cmd))
