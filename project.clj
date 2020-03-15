bbad38d9882ebf28e046e2f0bfe2fe165d025dab
Trevor Hartman <trevorhartman@gmail.com>
2020-03-15 11:20:28 -0600

    Remove accidental dupe yetibot/core dep

--- project.clj
+++ project.clj
@@ -46,7 +46,6 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [yetibot/core "20200302.181517.2c05eb0"]
-                 [yetibot/core "20200219.184121.8aef96d"]
 
                  ; apis
                  [twitter-api "1.8.0"]
