a4133a0b98c46a83912f250491af5cc513973148
mdave16 <madave@ebay.com>
2020-05-22 02:01:07 +0100

    Get xkcd comic by index

--- src/yetibot/commands/xkcd.clj
+++ src/yetibot/commands/xkcd.clj
@@ -3,13 +3,31 @@
     [yetibot.core.util.http :refer [get-json]]
     [yetibot.core.hooks :refer [cmd-hook]]))
 
-(def endpoint "http://xkcd.com/info.0.json")
+(defn- endpoint
+  ([] "http://xkcd.com/info.0.json")
+  ([i] (format "https://xkcd.com/%s/info.0.json" i)))
+
+(defn- format-xkcd-response
+  "Format response from xkcd"
+  [json]
+  ((juxt :title :img :alt) json))
 
 (defn xkcd-cmd
   "xkcd # fetch current xkcd comic"
   {:yb/cat #{:fun :img}}
   [_]
-  ((juxt :title :img :alt) (get-json endpoint)))
+  (format-xkcd-response (get-json (endpoint))))
+
+(defn xkcd-idx-cmd
+  "xkcd <index> # fetch xkcd number <index>"
+  {:yb/cat #{:fun :img}}
+  [{index :match}]
+  (format-xkcd-response
+    (try
+      (get-json (endpoint index))
+    (catch Exception _ 
+      (get-json (endpoint 1969))))))
 
 (cmd-hook #"xkcd"
+          #"\d+" xkcd-idx-cmd
           _ xkcd-cmd)
