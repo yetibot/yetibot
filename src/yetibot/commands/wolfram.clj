196f396878364c7be05fdf919cc39882e0340707
Anton Jeppsson <anton.jeppsson@outlook.com>
2020-07-16 21:29:29 +0200

    Improve wolfram command (#1031)
    * pass textual data and result data from wolfram command
    
    * add tests for wolfram command
    
    * use dynamic url building and pass error as :result/error for wolfram

--- src/yetibot/commands/wolfram.clj
+++ src/yetibot/commands/wolfram.clj
@@ -2,10 +2,11 @@
   (:require
     [clojure.spec.alpha :as s]
     [yetibot.core.util.http :refer [encode]]
-    [clojure.xml :as xml]
-    [taoensso.timbre :refer [info warn error]]
     [yetibot.core.config :refer [get-config]]
-    [yetibot.core.hooks :refer [cmd-hook]]))
+    [yetibot.core.hooks :refer [cmd-hook]]
+    [clojure.data.json :as json]
+    [clj-http.client :as client]
+    [clojure.string :as string]))
 
 (s/def ::appid string?)
 
@@ -13,20 +14,53 @@
 
 (def config (:value (get-config ::config [:wolfram])))
 (def app-id (:appid config))
-(def endpoint (str "http://api.wolframalpha.com/v2/query?appid=" app-id))
+(def endpoint "http://api.wolframalpha.com/v2/query")
 
-(defn parse-imgs-from-xml [xml]
-  (let [xs (xml-seq xml)]
-    (for [el xs :when (= :img (:tag el))] [(:alt (:attrs el)) (:src (:attrs el))])))
+(defn plaintext-or-image
+  "Return the subpods plaintext if present, otherwise it's image link."
+  [subpod]
+  (let [plaintext (:plaintext subpod)]
+    (if-not (empty? plaintext)
+      plaintext
+      (get-in subpod [:img :src]))))
+
+(defn data-string
+  "Concatenate pod title and all subpods' data."
+  [pod]
+  (->> pod
+       :subpods
+       (map plaintext-or-image)
+       (string/join "; ")
+       (str (:title pod) ": ")))
+
+(defn catch-suggestion
+  "Print some help if Wolfram can't interpret the query."
+  [wolfram-data]
+  (or (get-in wolfram-data [:tips :text])
+      (if-let [suggestion (get-in wolfram-data [:didyoumeans :val])]
+        (str "Did you mean " suggestion "?"))
+      (get-in wolfram-data [:languagemsg :english])
+      "Wolfram couldn't interpret your query."))
 
 (defn search-wolfram
   "wolfram <query> # search for <query> on Wolfram Alpha"
   {:yb/cat #{:info :img}}
   [{q :match}]
-  (flatten
-    (map #(str (second %) "&t=.jpg")
-         (parse-imgs-from-xml
-           (xml/parse (str endpoint "&input=" (encode q)))))))
+  (let [data (get-in (client/get endpoint
+                                       {:query-params {:input q
+                                                       :output "json"
+                                                       :appid app-id
+                                                       :reinterpret true}
+                                        :as :json})
+                     [:body :queryresult])]
+    (if (:success data)
+      {:result/value (map data-string (:pods data))
+       :result/collection-path [:pods]
+       :result/data data}
+      (if (:error data)
+        {:result/error (:error data)}
+        {:result/value (catch-suggestion data)
+         :result/data data}))))
 
 (cmd-hook #"wolfram"
   #".*" search-wolfram)
