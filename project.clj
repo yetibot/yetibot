c0e88c3b8c5e0bebeeeae275aa5c4b6cd25183c3
yetibot-admin <yetibot-admin@users.noreply.github.com>
2021-06-09 19:49:37 +0000

    Bump yetibot/core to "20210609.194756.5220bdc"

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
                    (println))}
 
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [yetibot/core "20210521.185125.6c55a34"]
+                 [yetibot/core "20210609.194756.5220bdc"]
 
                  ; apis
                  [twitter-api "1.8.0"]
