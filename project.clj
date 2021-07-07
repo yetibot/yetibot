f33d297f2118dc471aa50c913c213181026d9715
Yetibot <trevorhartman+yetibot@gmail.com>
2021-07-07 15:12:27 -0600

    Bump yetibot/core to "20210707.205015.30db5f4" (#1083)
    Co-authored-by: yetibot-admin <yetibot-admin@users.noreply.github.com>

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
                    (println))}
 
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [yetibot/core "20210706.205340.7a7949d"]
+                 [yetibot/core "20210707.205015.30db5f4"]
 
                  ; apis
                  [twitter-api "1.8.0"]
