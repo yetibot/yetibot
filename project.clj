6f0eb24fdb6806b0a603b603c0b00ba89c1a3a7c
yetibot-admin <yetibot-admin@users.noreply.github.com>
2023-12-22 22:09:18 +0000

    Bump yetibot/core to "20231222.220832.1078f45"

--- project.clj
+++ project.clj
@@ -52,7 +52,7 @@
                      "\u001B[m"))
                    (println))}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [yetibot/core "20231221.174313.1763e2b"]
+                 [yetibot/core "20231222.220832.1078f45"]
                  ; yetibot plugins. Note: we're baking these into the jar/docker
                  ; image instead of loading them dynamically at runtime because
                  ; we found that pomegranate does not work inside a docker
