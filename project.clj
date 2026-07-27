0d91345646a2ebb3f53c5041df909164bc5a7ac9
yetibot-admin <2448922+yetibot-admin@users.noreply.github.com>
2026-07-27 02:01:26 +0000

    Bump yetibot/core to "20260727.020029.2fc9d87"

--- project.clj
+++ project.clj
@@ -52,7 +52,7 @@
                      "\u001B[m"))
                    (println))}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [yetibot/core "20260727.014622.641d534"]
+                 [yetibot/core "20260727.020029.2fc9d87"]
                  ; not sure why we need to include this at the top level since
                  ; it's already included in yetibot/core, but things don't work
                  ; without it.
