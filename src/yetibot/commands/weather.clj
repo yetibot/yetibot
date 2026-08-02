893275e3cea2be0c9b5f60690c60fe33a0156b2b
Yetibot <yetibot@yetibot.com>
2026-08-02 14:49:06 +0000

    migrate weather command from Weatherbit.io to wttr.in

--- src/yetibot/commands/weather.clj
+++ src/yetibot/commands/weather.clj
@@ -5,61 +5,89 @@
     [taoensso.timbre :refer [info warn error]]
     [yetibot.core.config :refer [get-config]]
     [yetibot.core.hooks :refer [cmd-hook]]
-    [yetibot.models.postal-code :refer [chk-postal-code]]
     [yetibot.commands.weather.formatters :as fmt]))
 
 (s/def ::config any?)
 
 (def config (:value (get-config ::config [:weather :weatherbitio])))
 
-(def api-key (:key config))
 (def default-zip (-> config :default :zip))
 
 (defn get-json
-  [uri {:keys [query-params] :as opts}]
+  [location]
   (try
-    (let [options (merge {:as :json :coerce :always}
-                         opts
-                         {:query-params (merge {:key api-key :units "M"}
-                                               query-params)})
+    (let [uri (str "https://wttr.in/" (clojure.string/replace (or location "") #" " "+"))
+          options {:as :json :coerce :always :query-params {:format "j1"}}
           {:keys [status body]} (http.client/get uri options)]
       (condp = status
         200 body
-        204 {:error "Location not found."}))
+        {:error "Location not found."}))
     (catch Exception e
       (let [{:keys [status body]} (ex-data e)]
         (error "Request failed with status:" status)
-        body))))
+        (or body {:error "Failed to retrieve weather data."})))))
 
-(defn- endpoint
-  "API docs: https://www.weatherbit.io/api"
-  [path]
-  (str "https://api.weatherbit.io/v2.0/" path))
+(defn transform-current
+  [res]
+  (if-let [current (first (:current_condition res))]
+    (let [area (first (:nearest_area res))
+          city (some-> (get-in area [:areaName 0 :value]) clojure.string/trim)
+          state (some-> (get-in area [:region 0 :value]) clojure.string/trim)
+          raw-country (some-> (get-in area [:country 0 :value]) clojure.string/trim)
+          country (if (or (= raw-country "United States of America")
+                          (= raw-country "USA"))
+                    "US"
+                    (or raw-country ""))
+          temp-c (some-> (get current :temp_C) Float/parseFloat)
+          app-temp-c (some-> (get current :FeelsLikeC) Float/parseFloat)
+          wind-kmh (some-> (get current :windspeedKmph) Float/parseFloat)
+          wind-dir (some-> (get current :winddir16Point) clojure.string/trim)
+          desc (some-> (get-in current [:weatherDesc 0 :value]) clojure.string/trim)]
+      {:data [{:city_name city
+               :state_code state
+               :country_code country
+               :temp temp-c
+               :weather {:description desc}
+               :app_temp app-temp-c
+               :wind_spd wind-kmh
+               :wind_cdir wind-dir}]})
+    {:error "No current conditions found."}))
 
-(defn- get-by-name
-  "Get current conditions by location name str"
-  [path city]
-  (get-json (endpoint path) {:query-params {:city city}}))
+(defn transform-forecast
+  [res]
+  (if-let [weather-days (:weather res)]
+    (let [area (first (:nearest_area res))
+          city (some-> (get-in area [:areaName 0 :value]) clojure.string/trim)
+          state (some-> (get-in area [:region 0 :value]) clojure.string/trim)
+          raw-country (some-> (get-in area [:country 0 :value]) clojure.string/trim)
+          country (if (or (= raw-country "United States of America")
+                          (= raw-country "USA"))
+                    "US"
+                    (or raw-country ""))
+          forecast-data (mapv (fn [day]
+                                (let [min-temp (some-> (:mintempC day) Float/parseFloat)
+                                      max-temp (some-> (:maxtempC day) Float/parseFloat)
+                                      valid-date (:date day)
+                                      hourly (:hourly day)
+                                      noon-hour (first (filter #(= (:time %) "1200") hourly))
+                                      selected-hour (or noon-hour (first hourly))
+                                      desc (some-> (get-in selected-hour [:weatherDesc 0 :value]) clojure.string/trim)]
+                                  {:min_temp min-temp
+                                   :max_temp max-temp
+                                   :valid_date valid-date
+                                   :weather {:description desc}}))
+                              weather-days)]
+      {:city_name city
+       :state_code state
+       :country_code country
+       :data forecast-data})
+    {:error "No forecast data found."}))
 
-(defn- get-by-pc
-  "Get current conditions by post code and country code"
-  [path pc cc]
-  (get-json (endpoint path) {:query-params {:postal_code pc
-                                            :country cc}}))
-
-(defn- get-by-loc
-  "Attempt to parse out postal code and call the corresponding get-by-name or
-   get-by-pc function"
-  [path loc]
-  (let [[_ pc-or-loc maybe-cc] (re-matches #"(.+?)(?:,\s*([^,]+))?" (str loc))]
-    (if-let [[pc cc] (chk-postal-code pc-or-loc maybe-cc)]
-      (get-by-pc path pc cc)
-      (get-by-name path loc))))
-
-(defn- error-response [{:keys [error status_code status_message]}]
+(defn- error-response [result]
   (cond
-    error {:result/error error}
-    (= 429 status_code) {:result/error status_message}))
+    (nil? result) {:result/error "No response from weather service."}
+    (:error result) {:result/error (:error result)}
+    :else nil))
 
 (defn- format-current
   [formatters c]
@@ -70,15 +98,17 @@
 
 (defn current
   [loc]
-  (get-by-loc "current" loc))
+  (let [res (get-json loc)]
+    (if (:error res)
+      res
+      (transform-current res))))
 
 (defn forecast
   [loc]
-  (get-by-loc "forecast/daily" loc))
-
-(defn air
-  [loc]
-  (get-by-loc "current/airquality" loc))
+  (let [res (get-json loc)]
+    (if (:error res)
+      res
+      (transform-forecast res))))
 
 (defn parse-args
   "parse args to vec of unit kw and args str"
@@ -131,24 +161,12 @@
   "weather air <location> # look up current air conditions for <location> by name or postal code, optional country code"
   {:yb/cat #{:info}}
   [{[_ loc] :match}]
-  (let [result (air loc)]
-    (or
-     (error-response result)
-     (let [{[cs] :data city-name :city_name country-code :country_code} result
-           air-quality-data (->> (seq cs)
-                                 (map #(fmt/air-quality-item %))
-                                 sort)]
-       {:result/value (into [(format "%s (%s)" city-name country-code)] air-quality-data)
-        :result/data result}))))
+  {:result/error "Air quality lookup is no longer supported since migrating to wttr.in."})
 
 (defn default-air-cmd
   "weather air # look up current weather for default location"
   [_]
-  (if (and default-zip
-           (not-empty default-zip))
-    (air-cmd {:match default-zip})
-    {:result/error "A default zip code is not configured.
-                    Configure it at path weather.weatherbitio.default.zip"}))
+  {:result/error "Air quality lookup is no longer supported since migrating to wttr.in."})
 
 (cmd-hook #"weather"
           #"air\s+(.+)" air-cmd
